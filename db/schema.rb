eec09c1751b32dd2737f76c18baf1959bdec9ad0
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-06-20 13:28:50 +0200

    Rename image_description column to image_title on budget investments. [#1]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614111945) do
+ActiveRecord::Schema.define(version: 20170620105137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,7 +152,7 @@ ActiveRecord::Schema.define(version: 20170614111945) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.string   "image_description"
+    t.string   "image_title"
   end
 
   add_index "budget_investments", ["administrator_id"], name: "index_budget_investments_on_administrator_id", using: :btree
