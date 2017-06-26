dc40ebfb136d68e3929131e80971fc8ad560c1a3
taitus <sebastia.roig@gmail.com>
2017-06-26 22:23:31 +0200

    Add interestable behaviour to debates

--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -57,4 +57,56 @@ describe Interest do
     end
   end
 
+  describe "debate" do
+
+    let(:debate) { create(:debate) }
+
+    describe '.follow' do
+
+      it 'creates a interest when there is none' do
+        expect { described_class.follow(user, debate) }.to change{ Interest.count }.by(1)
+        expect(Interest.last.user).to eq(user)
+        expect(Interest.last.interestable).to eq(debate)
+      end
+
+      it 'does nothing if the interest already exists' do
+        described_class.follow(user, debate)
+        expect(described_class.follow(user, debate)).to eq(false)
+        expect(Interest.by_user_and_interestable(user, debate).count).to eq(1)
+      end
+
+      it 'increases the interest count' do
+        expect { described_class.follow(user, debate) }.to change{ debate.reload.interests_count }.by(1)
+      end
+    end
+
+    describe '.unfollow' do
+      it 'raises an error if the interest does not exist' do
+        expect(described_class.unfollow(user, debate)).to eq(false)
+      end
+
+      describe 'when the interest already exists' do
+        before(:each) { described_class.follow(user, debate) }
+
+        it 'removes an existing interest' do
+          expect { described_class.unfollow(user, debate) }.to change{ Interest.count }.by(-1)
+        end
+
+        it 'decreases the interest count' do
+          expect { described_class.unfollow(user, debate) }.to change{ debate.reload.interests_count }.by(-1)
+        end
+      end
+    end
+
+    describe '.interested?' do
+      it 'returns false when the user has not flagged the debate' do
+        expect(described_class.interested?(user, debate)).to_not be
+      end
+
+      it 'returns true when the user has interested the debate' do
+        described_class.follow(user, debate)
+        expect(described_class.interested?(user, debate)).to be
+      end
+    end
+  end
 end
