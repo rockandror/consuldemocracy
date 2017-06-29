79dbbb90fb387a480711aeba7842fa5961856591
Senén Rodero Rodríguez <senenrodero@gmail.com>
2017-06-29 13:25:52 +0200

    Fix

--- db/pro_seeds.rb
+++ db/pro_seeds.rb
@@ -68,7 +68,7 @@ def create_user(email, username = Faker::Name.name)
   )
 end
 
-admin = User.find.first
+admin = User.first
 admin.update(residence_verified_at: Time.current, confirmed_phone: Faker::PhoneNumber.phone_number, document_type: "1", verified_at: Time.current, document_number: "1111111111")
 
 moderator = create_user('mod@consul.dev', 'mod')
