5587498dca19bb35121219f7ce7c7a885db942f0
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-01-12 16:34:27 +0100

    Fix budget investment suggest feature

--- spec/features/budgets/investments_spec.rb
+++ spec/features/budgets/investments_spec.rb
@@ -817,7 +817,7 @@ feature 'Budget Investments' do
         visit new_budget_investment_path(budget)
         fill_in "Title", with: "search"
 
-        within("div#js-suggest") do
+        within("div.js-suggest") do
           expect(page).to have_content "You are seeing 5 of 6 investments containing the term 'search'"
         end
       end
@@ -832,7 +832,7 @@ feature 'Budget Investments' do
         visit new_budget_investment_path(budget)
         fill_in "Title", with: "item"
 
-        within('div#js-suggest') do
+        within('div.js-suggest') do
           expect(page).not_to have_content 'You are seeing'
         end
       end
@@ -847,7 +847,7 @@ feature 'Budget Investments' do
         visit new_budget_investment_path(other_budget)
         fill_in "Title", with: "search"
 
-        within('div#js-suggest') do
+        within('div.js-suggest') do
           expect(page).not_to have_content 'You are seeing'
         end
       end
