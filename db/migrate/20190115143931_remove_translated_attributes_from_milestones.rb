5444ccf06d7a7760fd3c5810858d4fbbcaf3e9a2
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-01-15 15:41:52 +0100

    Remove translatable attributes from Milestones
    This commit fixes the following globalize DEPRECATION WARNING thrown
    after gem update:
    
    "You have defined one or more translated attributes with names that
    conflict with column(s) on the model table. Globalize does not support
    this configuration anymore, remove or rename column(s) on the model
    table"

---
+++ db/migrate/20190115143931_remove_translated_attributes_from_milestones.rb
@@ -0,0 +1,6 @@
+class RemoveTranslatedAttributesFromMilestones < ActiveRecord::Migration
+  def change
+    remove_column :milestones, :title, :string
+    remove_column :milestones, :description, :text
+  end
+end
