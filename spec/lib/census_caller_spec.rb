409b1c448f47a7a5fc18404fd427f9f04d727435
taitus <sebastia.roig@gmail.com>
2019-04-17 14:05:48 +0200

    CustomCensusAP accept :date_of_birth and :postal_code
    New CustomCensusAPI allow receive :date_of_birth and
    :postal_code and use in request to endpoint always that have
    been configured on remote_census_configuration:
      - Setting["remote_census_request.alias_date_of_birth"]
      - Setting["remote_census_request.alias_postal_code"]
    
    Add new params to CensusCaller call method.

--- spec/lib/census_caller_spec.rb
+++ spec/lib/census_caller_spec.rb
@@ -18,7 +18,7 @@ describe CensusCaller do
       allow(CensusApi).to   receive(:call).with(1, "12345678A")
       allow(LocalCensus).to receive(:call).with(1, "12345678A")
 
-      response = api.call(1, "12345678A")
+      response = api.call(1, "12345678A", Date.parse("01/01/1983"), "28001")
 
       expect(response).to eq(local_census_response)
     end
@@ -38,7 +38,7 @@ describe CensusCaller do
       allow(CensusApi).to receive(:call).with(1, "12345678A")
       allow(LocalCensus).to receive(:call).with(1, "12345678A")
 
-      response = api.call(1, "12345678A")
+      response = api.call(1, "12345678A", Date.parse("01/01/1983"), "28001")
 
       expect(response).to eq(census_api_response)
     end
@@ -57,10 +57,10 @@ describe CensusCaller do
       expect_any_instance_of(CustomCensusApi).to receive(:call).and_return(custom_census_api_response)
       allow_any_instance_of(LocalCensus).to receive(:call).and_return(local_census_response)
 
-      allow(CustomCensusApi).to receive(:call).with(1, "12345678A")
+      allow(CustomCensusApi).to receive(:call).with(1, "12345678A", Date.parse("01/01/1983"), "28001")
       allow(LocalCensus).to receive(:call).with(1, "12345678A")
 
-      response = api.call(1, "12345678A")
+      response = api.call(1, "12345678A", Date.parse("01/01/1983"), "28001")
 
       expect(response).to eq(custom_census_api_response)
 
--- spec/lib/custom_census_api_spec.rb
+++ spec/lib/custom_census_api_spec.rb
@@ -20,21 +20,21 @@ describe CustomCensusApi do
     end
 
     it "returns the response for the first valid variant" do
-      allow(api).to receive(:get_response_body).with(1, "00123456").and_return(invalid_body)
-      allow(api).to receive(:get_response_body).with(1, "123456").and_return(invalid_body)
-      allow(api).to receive(:get_response_body).with(1, "0123456").and_return(valid_body)
+      allow(api).to receive(:get_response_body).with(1, "00123456", Date.parse("01/01/1983"), "28001").and_return(invalid_body)
+      allow(api).to receive(:get_response_body).with(1, "123456", Date.parse("01/01/1983"), "28001").and_return(invalid_body)
+      allow(api).to receive(:get_response_body).with(1, "0123456", Date.parse("01/01/1983"), "28001").and_return(valid_body)
 
-      response = api.call(1, "123456")
+      response = api.call(1, "123456", Date.parse("01/01/1983"), "28001")
 
       expect(response).to be_valid
       expect(response.date_of_birth).to eq(Date.new(1980, 1, 1))
     end
 
     it "returns the last failed response" do
-      allow(api).to receive(:get_response_body).with(1, "00123456").and_return(invalid_body)
-      allow(api).to receive(:get_response_body).with(1, "123456").and_return(invalid_body)
-      allow(api).to receive(:get_response_body).with(1, "0123456").and_return(invalid_body)
-      response = api.call(1, "123456")
+      allow(api).to receive(:get_response_body).with(1, "00123456", Date.parse("01/01/1983"), "28001").and_return(invalid_body)
+      allow(api).to receive(:get_response_body).with(1, "123456", Date.parse("01/01/1983"), "28001").and_return(invalid_body)
+      allow(api).to receive(:get_response_body).with(1, "0123456", Date.parse("01/01/1983"), "28001").and_return(invalid_body)
+      response = api.call(1, "123456", Date.parse("01/01/1983"), "28001")
 
       expect(response).not_to be_valid
     end
