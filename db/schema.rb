4f306baed5cf1ee3dd9023b5909a056bd35fa23a
taitus <sebastia.roig@gmail.com>
2019-05-14 11:12:29 +0200

    Add new fields to signature
    - Add :date_of_birth and :postal_code to Signature to allow send these
      fields to CustomCensusAPI
    
    - Add new model presence validates: Only validate :date_of_birth and
      :postal_code presence when the application has configured Remote Census
      and their alias fields has values.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190411090023) do
+ActiveRecord::Schema.define(version: 20190430003908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1259,6 +1259,8 @@ ActiveRecord::Schema.define(version: 20190411090023) do
     t.boolean  "verified",           default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.date     "date_of_birth"
+    t.string   "postal_code"
   end
 
   create_table "site_customization_content_blocks", force: :cascade do |t|
