dd487738e1117146959adb6130de0da533326960
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-05-24 16:54:37 +0200

    Rename LocalCensusRecord model to Census::Local

--- app/models/census/local.rb
+++ app/models/census/local.rb
@@ -1,4 +1,6 @@
-class LocalCensusRecord < ApplicationRecord
+class Census::Local < ApplicationRecord
+  self.table_name = "census_locals"
+
   validates :document_number, presence: true
   validates :document_type, presence: true
   validates :date_of_birth, presence: true
