68799b27932c0e48cff4f057e64ae80608138217
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-05-24 19:35:44 +0200

    Add LocalCensusRecords link to admin menu
    * Add to menu as a Settings sub-option
    * Add translations for EN and ES locales
    * Activate menu sub-option when user is at any Census::Local

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -51,8 +51,10 @@ module AdminHelper
   end
 
   def menu_settings?
-    ["settings", "tags", "geozones", "images", "content_blocks"].include?(controller_name) &&
-    controller.class.parent != Admin::Poll::Questions::Answers
+    controllers_names = ["settings", "tags", "geozones", "images", "content_blocks",
+      "locals"]
+    controllers_names.include?(controller_name) &&
+      controller.class.parent != Admin::Poll::Questions::Answers
   end
 
   def menu_customization?
