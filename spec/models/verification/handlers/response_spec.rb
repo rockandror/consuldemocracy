5c1338a1ce4c0307c44d00aac99da02d21c589d8
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-07-22 16:24:49 +0200

    Add Verification::Handlers:.Response class
    ... to manage handlers responses

---
+++ spec/models/verification/handlers/response_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+describe Verification::Handlers::Response do
+  describe "#success?" do
+    it "returns true when response is initialized to true" do
+      response = described_class.new(true, "success")
+
+      expect(response.success?).to be true
+    end
+
+    it "returns false when response is initialized to false" do
+      response = described_class.new(false, "error")
+
+      expect(response.success?).to be false
+    end
+  end
+end
