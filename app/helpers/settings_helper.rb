e88c746447746659f97c0a437cbe3c7a07db0ce4
taitus <sebastia.roig@gmail.com>
2019-07-23 14:05:44 +0200

    Create new setting page 'Advanced Configuration'
    Render all settings related with "http basic authentication"

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -13,4 +13,12 @@ module SettingsHelper
     key.first == "social" && key.last == "login"
   end
 
+  def advanced_feature?(setting)
+    key = setting.key.split(".")
+    key.first == "advanced" && key.last == "http_basic_auth"
+  end
+
+  def is_feature?(setting)
+    social_feature?(setting) || advanced_feature?(setting)
+  end
 end
