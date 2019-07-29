723046d13148d4eda6f2700e33b9fcfb18cd9a81
taitus <sebastia.roig@gmail.com>
2019-07-29 12:34:09 +0200

    Update and add specs

--- spec/features/admin/settings_spec.rb
+++ spec/features/admin/settings_spec.rb
@@ -96,6 +96,11 @@ describe "Admin settings" do
 
       expect(page).to have_content "Map configuration"
       expect(page).to have_css("#edit_setting_#{setting.id}")
+      expect(page).to have_content "Latitude"
+      expect(page).to have_content "Longitude"
+      expect(page).to have_content "Zoom"
+      expect(page).to have_content "TMS(Tile Map Service) Provider"
+      expect(page).to have_content "Attribution from TMS Provider"
     end
 
     scenario "Should display uploads settings section" do
@@ -197,9 +202,6 @@ describe "Admin settings" do
       end
 
       expect(page).to have_content "Map configuration updated succesfully"
-      within "#map-section" do
-        click_link "Configure"
-      end
       expect(find("#latitude", visible: false).value).not_to eq "51.48"
     end
 
