6b42e3cc091852c4808e6dd0b8de5110bb49475a
taitus <sebastia.roig@gmail.com>
2019-07-31 11:37:38 +0200

    Add Settings related with 'SMTP Configuration'

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -144,6 +144,14 @@ class Setting < ApplicationRecord
         "advanced.auth.http_basic_username": nil,
         "advanced.auth.http_basic_password": nil,
         "advanced.tracking.rollbar_server_token": nil,
+        # SMTP Configuration
+        "smtp.address": nil,
+        "smtp.port": nil,
+        "smtp.domain": nil,
+        "smtp.username": nil,
+        "smtp.password": nil,
+        "smtp.authentication": nil,
+        "smtp.enable_starttls_auto": nil,
         # Names for the moderation console, as a hint for moderators
         # to know better how to assign users with official positions
         "official_level_1_name": I18n.t("seeds.settings.official_level_1_name"),
