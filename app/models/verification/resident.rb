6b1ae46b74ab70aca7cef9fecd2199dc7ae56e56
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-09-09 11:48:30 +0200

    Add method to find verification residents by given data

--- app/models/verification/resident.rb
+++ app/models/verification/resident.rb
@@ -2,4 +2,8 @@ class Verification::Resident < ApplicationRecord
   self.table_name = "verification_residents"
 
   validates :data, presence: true, uniqueness: true
+
+  def self.find_by_data(data)
+    find_by("data @> ?", data.to_json)
+  end
 end
