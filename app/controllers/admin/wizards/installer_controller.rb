c683905e4e34d0f61ab101eec8bf0192ba8881c5
taitus <sebastia.roig@gmail.com>
2019-09-11 18:35:53 +0200

    Second commit
    rebaseme or die slowly

--- app/controllers/admin/wizards/installer_controller.rb
+++ app/controllers/admin/wizards/installer_controller.rb
@@ -1,10 +1,35 @@
 class Admin::Wizards::InstallerController < Admin::BaseController
+  layout "wizard"
+
+  before_action :set_total_steps
+  before_action :set_settings, only: :show
 
   def new
-    @wizard = @wizard.new
   end
 
   def show
+    render params[:id]
   end
+
+  private
+
+    def set_total_steps
+      @total_steps = 7
+    end
+
+    def set_settings
+      case params[:id]
+      when "general_settings"
+        @settings = [Setting.find_by(key: "org_name"), Setting.find_by(key: "min_age_to_participate")]
+      when "participation_process"
+        @settings = [[Setting.find_by(key: "process.debates"), Setting.find_by(key: "process.proposals"), Setting.find_by(key: "votes_for_proposal_success"), Setting.find_by(key: "process.polls"), Setting.find_by(key: "process.budgets")]]
+      when "map"
+        @settings = [[Setting.find_by(key: "feature.map"), Setting.find_by(key: "map.latitude"), Setting.find_by(key: "map.longitude"), Setting.find_by(key: "map.zoom")]]
+      when "smtp"
+        @settings = [Setting.find_by(key: "smpt."), Setting.find_by(key: "smtp.")]
+      when "regional"
+        @settings = [Setting.find_by(key: "regional."), Setting.find_by(key: "regional.")]
+      end
+    end
 
 end
