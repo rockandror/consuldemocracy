f3444561bb48bae3111e768805729978c4a59688
taitus <sebastia.roig@gmail.com>
2019-09-24 18:23:28 +0200

    Add general settings step

--- app/controllers/admin/wizards/installer_controller.rb
+++ app/controllers/admin/wizards/installer_controller.rb
@@ -2,14 +2,26 @@ class Admin::Wizards::InstallerController < Admin::BaseController
   layout "wizard"
 
   before_action :set_total_steps
+  before_action :set_settings, only: :show
 
   def new
+  end
+
+  def show
+    render params[:id]
   end
 
   private
 
     def set_total_steps
       @total_steps = 7
+    end
+
+    def set_settings
+      case params[:id]
+      when "general_settings"
+        @settings = [Setting.find_by(key: "org_name"), Setting.find_by(key: "min_age_to_participate")]
+      end
     end
 
 end
