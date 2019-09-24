d922f46509232a2252bca10a13bc15b84b12ec23
taitus <sebastia.roig@gmail.com>
2019-09-24 18:32:40 +0200

    Add regional configuration step
    In available locales section we allow you to disable all languages
    except the application default locale

--- app/controllers/admin/wizards/installer_controller.rb
+++ app/controllers/admin/wizards/installer_controller.rb
@@ -27,7 +27,14 @@ class Admin::Wizards::InstallerController < Admin::BaseController
         @settings = [[Setting.find_by(key: "feature.map"), Setting.find_by(key: "map.latitude"), Setting.find_by(key: "map.longitude"), Setting.find_by(key: "map.zoom")]]
       when "smtp"
         @settings = [Setting.find_by(key: "feature.smtp_configuration"), Setting.find_by(key: "smtp.address"), Setting.find_by(key: "smtp.port"), Setting.find_by(key: "smtp.domain"), Setting.find_by(key: "smtp.username"), Setting.find_by(key: "smtp.password"), Setting.find_by(key: "smtp.authentication"), Setting.find_by(key: "smtp.enable_starttls_auto")]
+      when "regional"
+        all_settings = Setting.all.group_by { |setting| setting.type }
+        @settings = [all_settings["regional.default_locale"]] + [valid_regional_available_locales(all_settings)] + [all_settings["regional.time_zone"]]
       end
+    end
+
+    def valid_regional_available_locales(all_settings)
+      all_settings["regional.available_locale"].select { |s| s.key.rpartition(".").last.to_sym != I18n.default_locale }
     end
 
 end
