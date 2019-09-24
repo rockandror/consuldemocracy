214acf701d175af1bf78c2ecb1b9490f21275d89
taitus <sebastia.roig@gmail.com>
2019-09-24 18:20:23 +0200

    Add initial step as action new from installation wizard

--- app/controllers/admin/wizards/installer_controller.rb
+++ app/controllers/admin/wizards/installer_controller.rb
@@ -1,3 +1,15 @@
 class Admin::Wizards::InstallerController < Admin::BaseController
   layout "wizard"
+
+  before_action :set_total_steps
+
+  def new
+  end
+
+  private
+
+    def set_total_steps
+      @total_steps = 7
+    end
+
 end
