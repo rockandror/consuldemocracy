44a8f61338a9bc58402b401cad12362ea1f80f70
taitus <sebastia.roig@gmail.com>
2019-09-25 10:38:55 +0200

    Clarify installer controller

--- app/controllers/admin/wizards/installer_controller.rb
+++ app/controllers/admin/wizards/installer_controller.rb
@@ -20,13 +20,28 @@ class Admin::Wizards::InstallerController < Admin::BaseController
     def set_settings
       case params[:id]
       when "general_settings"
-        @settings = [Setting.find_by(key: "org_name"), Setting.find_by(key: "min_age_to_participate")]
+        @settings = [Setting.find_by(key: "org_name"),
+                     Setting.find_by(key: "min_age_to_participate")]
       when "participation_process"
-        @settings = [[Setting.find_by(key: "process.debates"), Setting.find_by(key: "process.proposals"), Setting.find_by(key: "votes_for_proposal_success"), Setting.find_by(key: "process.polls"), Setting.find_by(key: "process.budgets")]]
+        @settings = [[Setting.find_by(key: "process.debates"),
+                      Setting.find_by(key: "process.proposals"),
+                      Setting.find_by(key: "votes_for_proposal_success"),
+                      Setting.find_by(key: "process.polls"),
+                      Setting.find_by(key: "process.budgets")]]
       when "map"
-        @settings = [[Setting.find_by(key: "feature.map"), Setting.find_by(key: "map.latitude"), Setting.find_by(key: "map.longitude"), Setting.find_by(key: "map.zoom")]]
+        @settings = [[Setting.find_by(key: "feature.map"),
+                      Setting.find_by(key: "map.latitude"),
+                      Setting.find_by(key: "map.longitude"),
+                      Setting.find_by(key: "map.zoom")]]
       when "smtp"
-        @settings = [Setting.find_by(key: "feature.smtp_configuration"), Setting.find_by(key: "smtp.address"), Setting.find_by(key: "smtp.port"), Setting.find_by(key: "smtp.domain"), Setting.find_by(key: "smtp.username"), Setting.find_by(key: "smtp.password"), Setting.find_by(key: "smtp.authentication"), Setting.find_by(key: "smtp.enable_starttls_auto")]
+        @settings = [Setting.find_by(key: "feature.smtp_configuration"),
+                     Setting.find_by(key: "smtp.address"),
+                     Setting.find_by(key: "smtp.port"),
+                     Setting.find_by(key: "smtp.domain"),
+                     Setting.find_by(key: "smtp.username"),
+                     Setting.find_by(key: "smtp.password"),
+                     Setting.find_by(key: "smtp.authentication"),
+                     Setting.find_by(key: "smtp.enable_starttls_auto")]
       when "regional"
         all_settings = Setting.all.group_by { |setting| setting.type }
         @settings = [all_settings["regional.default_locale"]] + [valid_regional_available_locales(all_settings)] + [all_settings["regional.time_zone"]]
