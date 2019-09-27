5db0aee6b2b37720679ceb43a28feab8996b3372
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-09-27 11:18:24 +0200

    Update confirmation
    1. Set confirmation fields needed to run all the confirmations
    2. Validates presence of all confimation codes
    3. Add validation to run all handler confirmations

--- app/models/verification/confirmation.rb
+++ app/models/verification/confirmation.rb
@@ -1,11 +1,13 @@
 class Verification::Confirmation
   include ActiveModel::Model
-  attr_accessor :fields, :user
+  attr_accessor :confirmation_fields, :user
 
   validates :user, presence: true
+  validate :confirmations, if: :confirmation_codes_present?
 
   def initialize(attributes = {})
-    @fields = define_confirmation_fields
+    @confirmation_fields = define_confirmation_fields
+    define_fields_validations
 
     super
   end
@@ -19,27 +21,55 @@ class Verification::Confirmation
     # Defines confirmation fields for each enabled handler that requires a
     # confirmation code
     def define_confirmation_fields
-      condition = Proc.new {|handler| active_handlers.include?(handler) && handler.requires_confirmation?}
-      handlers = Verification::Handler.descendants.select{condition}
-
-      handlers.each_with_object([]) do |handler, fields|
-        handler_name = handler.name.split('::').last.downcase
-        attr = "#{handler_name}_code"
-
-        define_singleton_method attr do
-          instance_variable_get "@#{attr}"
+      Verification::Configuration.confirmation_fields.each_with_object([]) do |confirmation_field, confirmation_fields|
+        define_singleton_method confirmation_field do
+          instance_variable_get "@#{confirmation_field}"
         end
 
-        define_singleton_method "#{attr}=" do |arg|
-          instance_variable_set "@#{attr}", arg
+        define_singleton_method "#{confirmation_field}=" do |arg|
+          instance_variable_set "@#{confirmation_field}", arg
         end
 
-        fields << attr
+        confirmation_fields << confirmation_field
       end
     end
 
-    # Return enabled handlers that requires confirmation code
-    def active_handlers
-      Verification::Field.all.where.not(handlers: [nil, ""]).pluck(:handlers).flatten.uniq
+    def confirmation_codes_present?
+      @confirmation_fields.all?{|confirmation_field| send(confirmation_field).present?}
+    end
+
+    # Validates only required confirmation handlers
+    def confirmations
+      Verification::Configuration.required_confirmation_handlers.each do |id, handler|
+        handler_instance = handler.new(fields_for_handler(handler))
+
+        response = handler_instance.confirm
+        unless response.success?
+          errors.add "#{handler.id.downcase.underscore}_confirmation_code", response.message
+        end
+      end
+    end
+
+    # Return {} of fields for given handler by handler name
+    def fields_for_handler(handler)
+      params = {}
+      @confirmation_fields.each do |confirmation_field|
+        params[confirmation_field] = send(confirmation_field)
+      end
+      params[:user] = user
+      params.symbolize_keys!
+    end
+
+    # Define self validations from existing verification fields
+    def define_fields_validations
+      define_presence_validations
+    end
+
+    def define_presence_validations
+      @confirmation_fields.each do |confirmation_field|
+        self.singleton_class.class_eval do
+          validates confirmation_field, presence: true
+        end
+      end
     end
 end
