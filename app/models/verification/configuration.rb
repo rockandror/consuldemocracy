ec6aa4e562d2e7810e22c15590c54bb26452e577
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-09-27 11:00:35 +0200

    Update verification configuration class
    Add methods to share them between other Verification models

--- app/models/verification/configuration.rb
+++ app/models/verification/configuration.rb
@@ -1,14 +1,34 @@
 class Verification::Configuration
-  def self.available_handlers
-    @@available_handlers ||= {}
-  end
 
-  def self.ids
-    available_handlers.keys.map(&:to_s)
-  end
+  class << self
+    def available_handlers
+      Verification::Handler.descendants.each_with_object({}) do |handler, hash|
+        hash[handler.id] = handler
+      end
+    end
 
-  def self.required_confirmation_handlers
-    @@available_handlers.select{|_, handler| handler.requires_confirmation?}
+    def ids
+      available_handlers.keys.map(&:to_s)
+    end
+
+    def active_handlers
+      Verification::Field.including_any_handlers(Verification::Configuration.ids).pluck(:handlers).flatten.uniq
+    end
+
+    def required_confirmation_handlers
+      available_handlers.select{|_, handler| handler.requires_confirmation?}
+    end
+
+    def confirmation_fields
+      condition = Proc.new {|handler| active_handlers.include?(handler) && handler.requires_confirmation?}
+      handlers = available_handlers.select{condition}
+
+      handlers.keys.each_with_object([]) do |handler, fields|
+        handler_name = handler.downcase.underscore
+
+        fields << "#{handler_name}_confirmation_code"
+      end
+    end
   end
 end
 Dir[Rails.root.join("app/models/verification/handlers/*.rb")].each {|file| require_dependency file }
--- spec/models/verification/configuration_spec.rb
+++ spec/models/verification/configuration_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe Verification::Configuration do
-  let(:handler) do
+  let!(:handler) do
     Class.new(Verification::Handler) do
       register_as :my_handler
     end
@@ -9,7 +9,23 @@ describe Verification::Configuration do
 
   describe ".available_handlers" do
     it "should return registered handlers" do
-      expect(described_class.available_handlers).to include({ "my_handler" => handler })
+      defined_handlers = { "my_handler" => handler, "sms" => Verification::Handlers::Sms }
+
+      expect(described_class.available_handlers).to include(defined_handlers)
+    end
+  end
+
+  describe ".ids" do
+    it "should return registered handlers ids" do
+      expect(described_class.ids).to include("sms", "my_handler")
+    end
+  end
+
+  describe ".active_handlers" do
+    it "should return only active handlers" do
+      create(:verification_field, name: "phone", handlers: [:sms, :my_handler])
+
+      expect(described_class.active_handlers).to eq(["sms", "my_handler"])
     end
   end
 
@@ -27,6 +43,24 @@ describe Verification::Configuration do
 
       expect(described_class.required_confirmation_handlers).to include({ "my_handler" => handler })
       expect(described_class.required_confirmation_handlers).not_to include without_confirmation_handler
+    end
+  end
+
+  describe ".confirmation_fields" do
+    let(:without_confirmation_handler) do
+      Class.new(Verification::Handler) do
+        register_as :my_handler
+      end
+    end
+
+    it "should return confirmation fields names for handlers in use" do
+      handler.class_eval do
+        requires_confirmation true
+      end
+
+      confirmation_fields = described_class.confirmation_fields
+
+      expect(confirmation_fields).to include("sms_confirmation_code", "my_handler_confirmation_code")
     end
   end
 end
