bcc2fe2b4c3575912202aa0965a450309cae5ad1
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-09-28 12:03:20 +0200

    Add data format validation when data is assigned as bad Hash string representation
    When HashSerializer throw the JSON::ParserError exception means that the
    string provided by user through the form cannot be parsed into a Hash. So
    we keep the bad formatted string and the format validation of data
    will take care to detect the error and to inform user.

--- spec/models/verification/resident_spec.rb
+++ spec/models/verification/resident_spec.rb
@@ -19,6 +19,13 @@ describe Verification::Resident do
     expect(resident).not_to be_valid
   end
 
+  it "Should not be valid when data is assigned as bad formatted hash string respresentation" do
+    resident.data = '{email=>"resident@email.com", document_number=>"12345678A"}'
+
+    expect(resident).not_to be_valid
+    expect(resident.errors[:data]).to include("Data format is invalid. An example of valid format could be: {\"key1\"=>\"value1\", \"key2\"=>\"value2\"}")
+  end
+
   it "Should not be valid when other record with exact data exists" do
     create(:verification_resident, data: { email: "email@example.com", document_number: "XYZ" })
     resident = build(:verification_resident, data: { email: "email@example.com", document_number: "XYZ" })
@@ -26,7 +33,26 @@ describe Verification::Resident do
     expect(resident).not_to be_valid
   end
 
+  context "save" do
+    it "Should convert well formatted hash string respresentation" do
+      resident = create(:verification_resident, data: '{"email"=>"resident@email.com", "document_number"=>"12345678A"}')
+
+      expect(resident).to be_persisted
+      expect(resident.data.class).to eq(Hash)
+    end
+  end
+
   context ".find_by_data" do
+
+    it "Should allow us to find a record created from data as string" do
+      resident = create(:verification_resident, data: '{"email"=>"resident@email.com", "document_number"=>"12345678A"}')
+      another_resident = create(:verification_resident, data: '{"email": "anotherresident@email.com", "document_number": "12345678B"}')
+
+      expect(described_class.find_by_data({ email: "resident@email.com" })).to eq(resident)
+      expect(described_class.find_by_data({ document_number: "12345678B" })).to eq(another_resident)
+      expect(described_class.find_by_data({ email: "resident@email.com", document_number: "12345678A" })).to eq(resident)
+    end
+
     it "Should allow us to find a record for any combination of information stored
         at data column" do
       resident = create(:verification_resident,
