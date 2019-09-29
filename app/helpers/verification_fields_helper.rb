c34fd49f04846fd58a6c7c59bdcb692c89557fe4
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-09-29 15:44:27 +0200

    Add handler text representation
    Add translations for EN and ES locales

--- app/helpers/verification_fields_helper.rb
+++ app/helpers/verification_fields_helper.rb
@@ -10,4 +10,16 @@ module VerificationFieldsHelper
   def field_handler_id(handler)
     "verification_field_handlers_#{handler}"
   end
+
+  def handlers_text_representation(field)
+    translations = []
+    field.handlers.each_with_object(translations) do |handler, translations|
+      translations << handler_text_representation(handler)
+    end
+    translations.sort
+  end
+
+  def handler_text_representation(handler)
+    t("admin.verification.fields.handlers.name.#{handler}")
+  end
 end
