758d655903efdec5bc1e17f4d677a3f0b6492333
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-02 17:22:44 +0200

    Move admin verification handler field assignments into "wizards" namespace

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -170,9 +170,6 @@ namespace :admin do
     get :search, on: :collection
   end
   namespace :verification do
-    resources :field_assignments, controller: "handler/field_assignments",
-                                  path: "handler/:handler_id/field_assignments",
-                                  as: "handler_field_assignments"
     namespace :handlers do
       namespace :sms do
         resources :fields, only: :create
@@ -278,11 +275,11 @@ namespace :admin do
   resources :wizards, only: :index
   namespace :wizards do
     resources :installer, only: [:new, :show]
-    resources :field_assignments, controller: "/admin/verification/handler/field_assignments",
-                                  path: "verification/handler/:handler_id/field_assignments",
-                                  as: "handler_field_assignments"
     namespace :verification do
       resources :fields
+      resources :field_assignments, controller: "handler/field_assignments",
+                                    path: "handler/:handler_id/field_assignments",
+                                    as: "handler_field_assignments"
     end
   end
 
