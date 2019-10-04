f84cb09cc2c70a7164f087be90b5ba2b563808a2
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-04 13:42:33 +0200

    Refactor verification handler model and model spec
    Now verify and confirm methods can receive the attributes from verification
    process and verification confirmation forms.
    
    Also fix rubocop offenses.

--- spec/models/verification/handler_spec.rb
+++ spec/models/verification/handler_spec.rb
@@ -18,8 +18,7 @@ describe Verification::Handler do
   end
 
   describe ".requires_confirmation?" do
-
-    it "should return true when explicitly enabled at handler" do
+    it "returns true when explicitly enabled at handler" do
       handler.class_eval do
         requires_confirmation true
       end
@@ -27,11 +26,11 @@ describe Verification::Handler do
       expect(handler.requires_confirmation?).to eq(true)
     end
 
-    it "should return false when not declared" do
+    it "returns false when not declared" do
       expect(handler.requires_confirmation?).to eq(false)
     end
 
-    it "should return false when explicitly disabled at handler" do
+    it "returns false when explicitly disabled at handler" do
       handler.class_eval do
         requires_confirmation false
       end
@@ -43,14 +42,15 @@ describe Verification::Handler do
   describe "#response" do
     subject { handler.new }
 
-    it "should be nil before calling verify method" do
+    it "is nil before calling verify method" do
       expect(subject.response).to be_blank
     end
 
-    it "should build a succesful as default response when handler does not implement
-        verify method" do
+    it "returns succesful response when handler does not implement verify method" do
 
-      expect{ subject.verify({}) }.to change{ subject.response }.from(nil).to(Verification::Handlers::Response)
+      expect {
+        subject.verify({})
+      }.to change { subject.response }.from(nil).to(Verification::Handlers::Response)
       expect(subject.success?).to be true
       expect(subject.success).to be true
     end
@@ -68,13 +68,17 @@ describe Verification::Handler do
         end
       end
 
-      it "should build a successful response when handler does implement verify method" do
-        expect{ subject.verify({email: "user@email.com", email_cofirmation: "user@email.com"}) }.to change{ subject.response }.from(nil).to(Verification::Handlers::Response)
+      it "returns successful response when handler does implement verify method" do
+        expect {
+          subject.verify({ email: "user@email.com", email_cofirmation: "user@email.com" })
+        }.to change { subject.response }.from(nil).to(Verification::Handlers::Response)
         expect(subject.success?).to be true
       end
 
-      it "should build an error response when handler does implement verify method" do
-        expect{ subject.verify({email: "user@email.com", email_cofirmation: "user@email.net"}) }.to change{ subject.response }.from(nil).to(Verification::Handlers::Response)
+      it "returns error response when handler does implement verify method" do
+        expect {
+          subject.verify({ email: "user@email.com", email_cofirmation: "user@email.net" })
+        }.to change { subject.response }.from(nil).to(Verification::Handlers::Response)
         expect(subject.success?).to be false
       end
     end
@@ -83,16 +87,6 @@ describe Verification::Handler do
   describe "#success" do
     subject { handler.new }
 
-    it "Should return nil when no response defined" do
-      expect(subject.success).to be_blank
-    end
-    it "Should return false when response is successful" do
-      subject = Verification::Handlers::Response.new(true, "success")
-      expect(subject.success).to be(true)
-    end
-    it "Should return false when response is error" do
-      subject = Verification::Handlers::Response.new(false, "error")
-      expect(subject.success).to be(false)
-    end
+    it { should respond_to(:success) }
   end
 end
