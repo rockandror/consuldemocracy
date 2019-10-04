bf9c000b4705d0c8a72ab47885547c8cab55fe1c
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-04 18:44:36 +0200

    Fix rubocop offenses

--- app/controllers/admin/wizards/verification_controller.rb
+++ app/controllers/admin/wizards/verification_controller.rb
@@ -2,12 +2,12 @@ class Admin::Wizards::VerificationController < Admin::BaseController
   layout "wizard"
 
   def new
-    all_settings = Setting.all.group_by { |setting| setting.type }
-    @settings = [Setting.find_by(key: "feature.custom_verification_process"), Setting.find_by(key: "feature.user.skip_verification"), Setting.find_by(key: "feature.remote_census")]
+    @settings = [Setting.find_by(key: "feature.custom_verification_process"),
+                 Setting.find_by(key: "feature.user.skip_verification"),
+                 Setting.find_by(key: "feature.remote_census")]
   end
 
   def finish
     @fields = ::Verification::Field.all.order(:position)
   end
-
 end
