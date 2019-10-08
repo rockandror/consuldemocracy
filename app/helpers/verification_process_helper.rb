279fc847595696b80e3e82faa237eea389f891df
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-08 16:06:31 +0200

    Use race condition

--- app/helpers/verification_process_helper.rb
+++ app/helpers/verification_process_helper.rb
@@ -1,12 +1,9 @@
 module VerificationProcessHelper
-
   def checkbox_label(field)
-    label = field.label
-    if field.checkbox_link.present?
-      slug = field.checkbox_link
-      custom_page = SiteCustomization::Page.find_by(slug: slug)
-      label += "(#{link_to('link', custom_page.url, target: '_blank')})"
-    end
-    label.html_safe
+    return field.label if field.checkbox_link.blank?
+
+    slug = field.checkbox_link
+    custom_page = SiteCustomization::Page.find_by(slug: slug)
+    "#{field.label} (#{link_to('link', custom_page.url, target: '_blank')})".html_safe
   end
 end
