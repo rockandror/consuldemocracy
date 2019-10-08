b9f1537a16fe46ce23ed2fb8c814440e9dbbd488
taitus <sebastia.roig@gmail.com>
2019-10-08 15:11:51 +0200

    Fix spec. Allow checkbox fields without related link.

--- app/helpers/verification_process_helper.rb
+++ app/helpers/verification_process_helper.rb
@@ -1,7 +1,12 @@
 module VerificationProcessHelper
 
   def checkbox_label(field)
-    label = "#{field.label}"
-    label += "(#{link_to("link", SiteCustomization::Page.find_by(slug: field.checkbox_link).url, target: "_blank")})".html_safe if field.checkbox_link.present?
+    label = field.label
+    if field.checkbox_link.present?
+      slug = field.checkbox_link
+      custom_page = SiteCustomization::Page.find_by(slug: slug)
+      label += "(#{link_to('link', custom_page.url, target: '_blank')})"
+    end
+    label.html_safe
   end
 end
