3498abec30c249b69ba1eae489c3fc52c0b63623
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-09 11:29:39 +0200

    Add missing translations

--- app/helpers/verification_fields_helper.rb
+++ app/helpers/verification_fields_helper.rb
@@ -26,4 +26,16 @@ module VerificationFieldsHelper
   def field_hint(field)
     field.hint || false
   end
+
+  def verification_fields_no_results_message
+    t("admin.wizards.verification.fields.index.no_results_html",
+      link: link_to(t("admin.wizards.verification.fields.index.create_first"),
+                    new_admin_wizards_verification_field_path))
+  end
+
+  def verification_fields_no_fields_defined
+    t("admin.wizards.verification.finish.no_fields_defined_html",
+      link: link_to(t("admin.wizards.verification.finish.skip_user_verification"),
+                    new_admin_wizards_verification_path))
+  end
 end
