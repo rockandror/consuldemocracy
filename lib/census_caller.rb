8a122bfa0be0dd8235a68816b3561499ede4542b
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-18 10:12:56 +0200

    Use setting to decide to call or not the legacy census api

--- lib/census_caller.rb
+++ lib/census_caller.rb
@@ -1,12 +1,13 @@
 class CensusCaller
 
   def call(document_type, document_number, date_of_birth, postal_code)
-    if Setting["feature.remote_census"].present?
-      response = RemoteCensusApi.new.call(document_type, document_number, date_of_birth, postal_code)
-    else
-      response = CensusApi.new.call(document_type, document_number)
-    end
-    response = LocalCensus.new.call(document_type, document_number) unless response.valid?
+    response = if Setting["feature.remote_census"].present?
+                 RemoteCensusApi.new.call(document_type, document_number, date_of_birth, postal_code)
+               elsif Setting["feature.legacy_remote_census"].present?
+                 CensusApi.new.call(document_type, document_number)
+               end
+
+    response = LocalCensus.new.call(document_type, document_number) unless response&.valid?
 
     response
   end
