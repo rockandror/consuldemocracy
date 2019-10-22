34766a6a93071c6e404e04687593ed31f2b700b8
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-22 15:51:02 +0200

    Simplify specs group

--- spec/models/verification/handler_spec.rb
+++ spec/models/verification/handler_spec.rb
@@ -139,45 +139,34 @@ describe Verification::Handler do
   describe "#response" do
     subject { handler.new }
 
+    before do
+      handler.class_eval do
+        def verify(params)
+          if params[:email] == params[:email_cofirmation]
+            @response = Verification::Handlers::Response.new(true, "success", params, {})
+          else
+            @response = Verification::Handlers::Response.new(false, "error", params, {})
+          end
+        end
+      end
+    end
+
     it "is nil before calling verify method" do
       expect(subject.response).to be_blank
     end
 
-    it "returns succesful response when handler does not implement verify method" do
-
+    it "returns error response when handler after call verify with correct verification information" do
       expect do
-        subject.verify({})
+        subject.verify({ email: "user@email.com", email_cofirmation: "user@email.com" })
       end.to change { subject.response }.from(nil).to(Verification::Handlers::Response)
       expect(subject.success?).to be true
-      expect(subject.success).to be true
     end
 
-    describe "when verify method is overriden by handler" do
-      before do
-        handler.class_eval do
-          def verify(params)
-            if params[:email] == params[:email_cofirmation]
-              @response = Verification::Handlers::Response.new(true, "success", params, {})
-            else
-              @response = Verification::Handlers::Response.new(false, "error", params, {})
-            end
-          end
-        end
-      end
-
-      it "returns successful response when handler does implement verify method" do
-        expect do
-          subject.verify({ email: "user@email.com", email_cofirmation: "user@email.com" })
-        end.to change { subject.response }.from(nil).to(Verification::Handlers::Response)
-        expect(subject.success?).to be true
-      end
-
-      it "returns error response when handler does implement verify method" do
-        expect do
-          subject.verify({ email: "user@email.com", email_cofirmation: "user@email.net" })
-        end.to change { subject.response }.from(nil).to(Verification::Handlers::Response)
-        expect(subject.success?).to be false
-      end
+    it "returns error response when handler after call verify with wrong verification information" do
+      expect do
+        subject.verify({ email: "user@email.com", email_cofirmation: "user@email.net" })
+      end.to change { subject.response }.from(nil).to(Verification::Handlers::Response)
+      expect(subject.success?).to be false
     end
   end
 
