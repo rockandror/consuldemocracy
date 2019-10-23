b98d728ddcfa1bed4b3e9db342e56891f70162dc
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-23 17:41:52 +0200

    Solve offensses

--- spec/models/verification/field/option_spec.rb
+++ spec/models/verification/field/option_spec.rb
@@ -3,20 +3,19 @@ require "rails_helper"
 describe Verification::Field::Option do
   let(:option) { build(:verification_field_option) }
 
-  it "Should be valid" do
+  it "is valid" do
     expect(option).to be_valid
   end
 
-  it "Should not be valid without label" do
+  it "is not valid without a label" do
     option.label = nil
 
     expect(option).not_to be_valid
   end
 
-  it "Should not be valid without value" do
+  it "is not valid without a value" do
     option.value = nil
 
     expect(option).not_to be_valid
   end
-
 end
