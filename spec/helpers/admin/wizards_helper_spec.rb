9c8e08176bdce8fd603def45c09e9405ddfcf359
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-23 16:45:00 +0200

    Remove unneeded empty lines and solve 2 offenses

--- spec/helpers/admin/wizards_helper_spec.rb
+++ spec/helpers/admin/wizards_helper_spec.rb
@@ -1,22 +1,21 @@
 require "rails_helper"
 
 describe Admin::WizardsHelper do
-
   context "#next_step_path" do
-
     describe "when current step is handlers index" do
-
       it "return remote_census step path if census soap Setting is enabled" do
         Setting["custom_verification_process.remote_census"] = true
 
-        expect(next_step_path("handlers")).to eq(admin_wizards_verification_field_assignments_path(:remote_census))
+        expect(next_step_path("handlers")).
+          to eq(admin_wizards_verification_field_assignments_path(:remote_census))
       end
 
       it "return resident step path if census soap Setting is disabled and census local is enabled" do
         Setting["custom_verification_process.remote_census"] = false
         Setting["custom_verification_process.residents"] = true
 
-        expect(next_step_path("handlers")).to eq(admin_wizards_verification_field_assignments_path(:residents))
+        expect(next_step_path("handlers")).
+          to eq(admin_wizards_verification_field_assignments_path(:residents))
       end
 
       it "return sms step path if census soap and local Settings are disabled and sms is enabled" do
@@ -34,15 +33,14 @@ describe Admin::WizardsHelper do
 
         expect(next_step_path("handlers")).to eq(admin_wizards_verification_finish_path)
       end
-
     end
 
     describe "when current step is remote_census" do
-
       it "return resident step path if census local Setting is enabled" do
         Setting["custom_verification_process.residents"] = true
 
-        expect(next_step_path("remote_census")).to eq(admin_wizards_verification_field_assignments_path(:residents))
+        expect(next_step_path("remote_census")).
+          to eq(admin_wizards_verification_field_assignments_path(:residents))
       end
 
       it "return sms step path if census local Setting is disabled and sms is enabled" do
@@ -58,11 +56,9 @@ describe Admin::WizardsHelper do
 
         expect(next_step_path("remote_census")).to eq(admin_wizards_verification_finish_path)
       end
-
     end
 
     describe "when current step is resident" do
-
       it "return sms step path if sms Setting is enabled" do
         Setting["custom_verification_process.sms"] = true
 
@@ -74,35 +70,28 @@ describe Admin::WizardsHelper do
 
         expect(next_step_path("residents")).to eq(admin_wizards_verification_finish_path)
       end
-
     end
 
     describe "when current step is sms" do
-
       it "return finish_path" do
         expect(next_step_path("sms")).to eq(admin_wizards_verification_finish_path)
       end
-
     end
-
   end
 
   context "#back_step_path" do
-
     describe "when current step is remote_census" do
-
       it "return handlers index path" do
         expect(back_step_path("remote_census")).to eq(admin_wizards_verification_fields_path)
       end
-
     end
 
     describe "when current step is resident" do
-
       it "return remote_census step path if remote_census Setting is enabled" do
         Setting["custom_verification_process.remote_census"] = true
 
-        expect(back_step_path("residents")).to eq(admin_wizards_verification_field_assignments_path(:remote_census))
+        expect(back_step_path("residents")).
+          to eq(admin_wizards_verification_field_assignments_path(:remote_census))
       end
 
       it "return handler index path if remote_census Setting is disabled" do
@@ -110,11 +99,9 @@ describe Admin::WizardsHelper do
 
         expect(back_step_path("residents")).to eq(admin_wizards_verification_fields_path)
       end
-
     end
 
     describe "when current step is sms" do
-
       it "return resident step path if residents Setting is enabled" do
         Setting["custom_verification_process.residents"] = true
 
@@ -134,40 +121,34 @@ describe Admin::WizardsHelper do
 
         expect(back_step_path("sms")).to eq(admin_wizards_verification_fields_path)
       end
-
     end
-
   end
 
   describe "#display_checkbox_link" do
-
     it "return display none when field is not checkbox kind" do
-      text_field =  create(:verification_field)
+      text_field = create(:verification_field)
 
       expect(display_checkbox_link(text_field)).to eq "display:none"
     end
 
     it "return blank when field is checkbox kind" do
-      checkbox_field =  create(:verification_field, kind: "checkbox")
+      checkbox_field = create(:verification_field, kind: "checkbox")
 
       expect(display_checkbox_link(checkbox_field)).to eq nil
     end
-
   end
 
   describe "#display_field_verification_options_section" do
-
     it "return display none when field is not selector kind" do
-      text_field =  create(:verification_field)
+      text_field = create(:verification_field)
 
       expect(display_field_verification_options_section(text_field)).to eq "display:none"
     end
 
     it "return blank when field is selector kind" do
-      selector_field =  create(:verification_field, kind: "selector")
+      selector_field = create(:verification_field, kind: "selector")
 
       expect(display_field_verification_options_section(selector_field)).to eq nil
     end
-
   end
 end
