4593e2a9a2c799129a401d2838c84f1d8c136b30
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-23 17:42:08 +0200

    Solve offensses

--- spec/models/verification/residents/import_spec.rb
+++ spec/models/verification/residents/import_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 describe Verification::Residents::Import do
-
   let(:base_files_path) { %w[spec fixtures files verification residents import] }
   let(:import) { build(:verification_residents_import) }
 
@@ -36,8 +35,8 @@ describe Verification::Residents::Import do
 
   context "#save" do
     it "Create valid verification resident with provided values" do
-      import.save
-      verification_resident = Verification::Resident.find_by_data({document_number: "44556678T"})
+      import.save!
+      verification_resident = Verification::Resident.find_by_data({ document_number: "44556678T" })
 
       expect(verification_resident).not_to be_nil
       expect(verification_resident.data["document_type"]).to include("DNI")
@@ -46,28 +45,29 @@ describe Verification::Residents::Import do
     end
 
     it "Add successfully created verification residents to created_records array" do
-      import.save
+      import.save!
 
       valid_document_numbers = ["44556678T", "33556678T", "22556678T", "X11556678"]
-      expect(import.created_records.collect{|record| record.data["document_number"]}).to eq(valid_document_numbers)
+      expect(import.created_records.collect { |record| record.data["document_number"] }).
+        to eq(valid_document_numbers)
     end
 
-    it "Add invalid (suplicated) verification residents to invalid_records array" do
+    it "Add invalid (duplicated) verification residents to invalid_records array" do
       path = base_files_path << "invalid.csv"
       file = Rack::Test::UploadedFile.new(Rails.root.join(*path))
       import.file = file
 
-      import.save
+      import.save!
 
-      invalid_records_document_types = ["DNI","NIE"]
+      invalid_records_document_types = ["DNI", "NIE"]
       invalid_records_document_numbers = ["44556678T", "X11556678"]
       invalid_records_house_ids = ["000000001", "000000004"]
 
-      expect(import.invalid_records.collect{|record| record.data["document_type"]})
+      expect(import.invalid_records.collect { |record| record.data["document_type"] })
         .to eq(invalid_records_document_types)
-      expect(import.invalid_records.collect{|record| record.data["document_number"]})
+      expect(import.invalid_records.collect { |record| record.data["document_number"] })
         .to eq(invalid_records_document_numbers)
-      expect(import.invalid_records.collect{|record| record.data["house_id"]})
+      expect(import.invalid_records.collect { |record| record.data["house_id"] })
         .to eq(invalid_records_house_ids)
     end
   end
