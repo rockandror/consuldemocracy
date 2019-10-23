be92c54f97132ceef88a16986ad82f0bdc169828
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-23 17:41:35 +0200

    Solve offensses

--- app/models/verification/handlers/response.rb
+++ app/models/verification/handlers/response.rb
@@ -1,5 +1,5 @@
 class Verification::Handlers::Response
-  attr_reader :message, :submitted_data, :submitted_data, :response_data, :success
+  attr_reader :message, :submitted_data, :response_data, :success
   alias :success? :success
 
   def initialize(success, message, submitted_data = {}, response_data = {})
