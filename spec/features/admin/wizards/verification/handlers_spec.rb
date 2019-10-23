ae9d8aecca8448d6bd41c67251a0752d6501c683
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-23 17:23:02 +0200

    Solve offensses

--- spec/features/admin/wizards/verification/handlers_spec.rb
+++ spec/features/admin/wizards/verification/handlers_spec.rb
@@ -1,11 +1,9 @@
 require "rails_helper"
 
 describe "Wizard Verification Handlers" do
+  let(:admin) { create(:administrator) }
 
-  before do
-    @admin = create(:administrator)
-    login_as(@admin.user)
-  end
+  before { login_as(admin.user) }
 
   context "Index" do
     scenario "Display default custom verification process settings" do
@@ -15,14 +13,16 @@ describe "Wizard Verification Handlers" do
       expect(page).to have_content "Configure the remote census to be used during user verification process."
 
       expect(page).to have_content "Verify a user against the Local Census"
-      expect(page).to have_content "Configure the local census to be used in the verification of a user in the application."
+      expect(page).to have_content "Configure the local census to be used in the verification " \
+                                   "of a user in the application."
 
       expect(page).to have_content "Verify a user's phone"
-      expect(page).to have_content "Configure user's phone verification to use it during the verification process. "
+      expect(page).to have_content "Configure user's phone verification to use it during the " \
+                                   "verification process. "
     end
 
     scenario "Allow enable a new custom verification process setting" do
-      setting = Setting.create(key: "custom_verification_process.sample_key")
+      setting = Setting.create!(key: "custom_verification_process.sample_key")
 
       visit admin_wizards_verification_handlers_path
       find("#edit_setting_#{setting.id} .button").click
@@ -30,5 +30,4 @@ describe "Wizard Verification Handlers" do
       expect(page).to have_content "Value updated"
     end
   end
-
 end
