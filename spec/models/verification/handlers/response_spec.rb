f80cac5ff1e3ef3638d65cc341d029451e57da17
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-23 17:42:02 +0200

    Solve offensses

--- spec/models/verification/handlers/response_spec.rb
+++ spec/models/verification/handlers/response_spec.rb
@@ -1,15 +1,17 @@
 require "rails_helper"
 
 describe Verification::Handlers::Response do
+  let(:model) { Verification::Handlers::Response }
+
   describe "#success?" do
     it "returns true when response is initialized to true" do
-      response = described_class.new(true, "success")
+      response = model.new(true, "success")
 
       expect(response.success?).to be true
     end
 
     it "returns false when response is initialized to false" do
-      response = described_class.new(false, "error")
+      response = model.new(false, "error")
 
       expect(response.success?).to be false
     end
@@ -17,13 +19,13 @@ describe Verification::Handlers::Response do
 
   describe "#error?" do
     it "returns true when response is initialized to true" do
-      response = described_class.new(true, "success")
+      response = model.new(true, "success")
 
       expect(response.error?).to be false
     end
 
     it "returns false when response is initialized to false" do
-      response = described_class.new(false, "error")
+      response = model.new(false, "error")
 
       expect(response.error?).to be true
     end
