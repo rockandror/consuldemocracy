b66dc4c2ce385a1d6d672b553430cac3ccadd107
taitus <sebastia.roig@gmail.com>
2019-10-25 16:51:22 +0200

    Resolve pull with stashed changes

--- app/helpers/verification_fields_helper.rb
+++ app/helpers/verification_fields_helper.rb
@@ -1,16 +1,4 @@
 module VerificationFieldsHelper
-  def field_handlers
-    Verification::Configuration.available_handlers
-  end
-
-  def field_handler_enabled?(field, handler)
-    field.handlers.present? ? field.handlers.include?(handler.to_s) : false
-  end
-
-  def field_handler_id(handler)
-    "verification_field_handlers_#{handler}"
-  end
-
   def handlers_text_representation(field)
     field.handlers.each_with_object([]) do |handler, translations|
       translations << handler_text_representation(handler)
@@ -18,7 +6,7 @@ module VerificationFieldsHelper
   end
 
   def handler_text_representation(handler)
-    t("admin.wizards.verification.fields.handlers.name.#{handler}")
+    t("admin.wizards.verification.fields.field.handlers.#{handler}")
   end
 
   def field_hint(field)
