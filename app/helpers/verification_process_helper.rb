cc3e944017ea361106d052d8fb04ededf4d43272
Senén Rodero Rodríguez <senenrodero@gmail.com>
2019-10-25 09:26:58 +0200

    Rename methods

--- app/helpers/verification_process_helper.rb
+++ app/helpers/verification_process_helper.rb
@@ -3,15 +3,15 @@ module VerificationProcessHelper
     SiteCustomization::Page.find_by(slug: field.checkbox_link).url
   end
 
-  def process_field_name(field)
+  def process_confirmation_field_name(field)
     "#{field.name}_confirmation"
   end
 
-  def process_field_label(field)
+  def process_confirmation_field_label(field)
     t("verification.process.form.confirmation_label", original_label: field.label)
   end
 
-  def process_field_hint(field)
+  def process_confirmation_field_hint(field)
     t("verification.process.form.confirmation_hint", original_label: field.label.downcase)
   end
 end
