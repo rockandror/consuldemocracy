7511f64f05b87c0f11383d34f76f70b2700aad0a
Julian Herrero <microweb10@gmail.com>
2019-10-31 11:07:24 +0700

    Update chromeOptions for newer versions of chromedriver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -44,7 +44,7 @@ end
 
 Capybara.register_driver :headless_chrome do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: %w(headless no-sandbox window-size=1200,600) }
+    "goog:chromeOptions" => { args: %w(headless no-sandbox window-size=1200,600) }
   )
 
   Capybara::Selenium::Driver.new(
