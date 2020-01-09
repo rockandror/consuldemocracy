76b161b966ad431d5d1d18a1edac09601a66dbf0
Angel Perez <iAngel.p93@gmail.com>
2020-01-09 12:21:36 -0400

    Allow admins to add moderated words

--- app/controllers/admin/moderated_texts_controller.rb
+++ app/controllers/admin/moderated_texts_controller.rb
@@ -8,4 +8,22 @@ class Admin::ModeratedTextsController < Admin::BaseController
     @word = ModeratedText.new
   end
 
+  def create
+    @word = ModeratedText.new(moderated_text_params)
+
+    if @word.save
+      notice = t("admin.moderated_texts.create.notice")
+      redirect_to admin_moderated_texts_path, notice: notice
+    else
+      flash.now[:error] = t("admin.moderated_texts.create.error")
+      render :new
+    end
+  end
+
+  private
+
+    def moderated_text_params
+      params.require(:moderated_text).permit(:text)
+    end
+
 end
