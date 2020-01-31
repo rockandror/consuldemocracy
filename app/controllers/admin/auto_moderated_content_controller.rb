ad6017683b524b5a30a5623d6668cf6d7e249f7e
Angel Perez <iAngel.p93@gmail.com>
2020-01-30 20:06:39 -0400

    Use filter_subnav to filter auto moderated content

--- app/controllers/admin/auto_moderated_content_controller.rb
+++ app/controllers/admin/auto_moderated_content_controller.rb
@@ -1,6 +1,8 @@
 class Admin::AutoModeratedContentController < Admin::BaseController
   VALID_FILTERS = %w[confirmed declined].freeze
 
+  has_filters %w{all confirmed declined}, only: :index
+
   def index
     load_moderated_contents
     @moderated_comments = ::Comment.where(id: @moderated_contents).includes(:moderated_texts, :user)
@@ -8,14 +10,14 @@ class Admin::AutoModeratedContentController < Admin::BaseController
 
   private
     def load_moderated_contents
-      if params[:moderation_status].present? && is_valid_filter?
-        @moderated_contents = ::ModeratedContent.send("#{params[:moderation_status]}").pluck(:moderable_id)
+      if params[:filter].present? && is_valid_filter?
+        @moderated_contents = ::ModeratedContent.send("#{params[:filter]}").pluck(:moderable_id)
       else
         @moderated_contents = ::ModeratedContent.all.pluck(:moderable_id)
       end
     end
 
     def is_valid_filter?
-      VALID_FILTERS.include?(params[:moderation_status])
+      VALID_FILTERS.include?(params[:filter])
     end
 end
