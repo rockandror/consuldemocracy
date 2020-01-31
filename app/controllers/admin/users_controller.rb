f70d729ea96e1f7191ef5e28fa8be17bf4556b09
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2020-01-31 09:34:16 +0100

    [DEMAD_18] Eliminación de usuarios

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,40 +11,78 @@ class Admin::UsersController < Admin::BaseController
   end
 
   def destroy
-    id_aux = @user.id
     begin
       ActiveRecord::Base.transaction do
-        Administrator.where(user_id: id_aux).each do |admin|
+        Administrator.where(user_id: @user.id).each do |admin|
           admin.destroy
         end
-        Valuator.where(user_id: id_aux).each do |valuator|
+        Proposal.where(author_id: @user.id).each do |proposal|
+          proposal.destroy
+        end
+        Debate.where(author_id: @user.id).each do |debate|
+          debate.destroy
+        end
+        Activity.where(user_id: @user.id).each do |activity|
+          activity.destroy
+        end
+        Budget::Investment.where(author_id: @user.id).each do |b_i|
+          b_i.destroy
+        end
+        Comment.where(user_id: @user.id).each do |comment|
+          comment.destroy
+        end
+        FailedCensusCall.where(user_id: @user.id).each do |call|
+          call.destroy
+        end
+        Valuator.where(user_id: @user.id).each do |valuator|
           valuator.destroy
         end
-        Moderator.where(user_id: id_aux).each do |valuator|
+        DirectMessage.where(sender_id: @user.id).each do |dm|
+          dm.destroy
+        end
+        DirectMessage.where(receiver_id: @user.id).each do |dm|
+          dm.destroy
+        end
+        Moderator.where(user_id: @user.id).each do |valuator|
           valuator.destroy
         end
-        Manager.where(user_id: id_aux).each do |valuator|
+        Legislation::Answer.where(user_id: @user.id).each do |legis|
+          legis.destroy
+        end
+        Manager.where(user_id: @user.id).each do |valuator|
           valuator.destroy
         end
-        Organization.where(user_id: id_aux).each do |org|
+        Organization.where(user_id: @user.id).each do |org|
           org.destroy
         end
-        Lock.where(user_id: id_aux).each do |lock|
+        Follow.where(user_id: @user.id).each do |org|
+          org.destroy
+        end
+        Lock.where(user_id: @user.id).each do |lock|
           lock.destroy
         end
-        Image.where(user_id: id_aux).each do |image|
+        Image.where(user_id: @user.id).each do |image|
           image.destroy
         end
-        Flag.where(user_id: id_aux).each do |flag|
+        Flag.where(user_id: @user.id).each do |flag|
           flag.destroy
         end
-        Notification.where(user_id: id_aux).each do |notification|
+        Notification.where(user_id: @user.id).each do |notification|
           notification.destroy
         end
-        Identity.where(user_id: id_aux).each do |identity|
+        Identity.where(user_id: @user.id).each do |identity|
           identity.destroy
         end
-        Poll::Question.where(author_id: id_aux).each do |poll|
+        RelatedContentScore.where(user_id: @user.id).each do |related|
+          related.destroy
+        end
+        Document.where(user_id: @user.id).each do |doc|
+          doc.destroy
+        end
+        Dashboard::AdministratorTask.where(user_id: @user.id).each do |dash|
+          dash.destroy
+        end
+        Poll::Question.where(author_id: @user.id).each do |poll|
           poll_question_answers = "delete from poll_question_answers where question_id = #{poll.id}"
           ActiveRecord::Base.connection.execute(poll_question_answers)
           poll_partial_results = "delete from poll_partial_results where question_id = #{poll.id}"
@@ -54,10 +92,10 @@ class Admin::UsersController < Admin::BaseController
           poll_questions = "delete from poll_questions where id = #{poll.id}"
           ActiveRecord::Base.connection.execute(poll_questions)
         end
-        Poll::Officer.where(user_id: id_aux).each do |poll|
+        Poll::Officer.where(user_id: @user.id).each do |poll|
           poll.destroy
         end
-        delete_users = "delete from users where id = #{id_aux}"
+        delete_users = "delete from users where id = #{@user.id}"
         ActiveRecord::Base.connection.execute(delete_users)
         flash[:notice] = t("admin.users.user.successfuly_deleted")
       end
