ee1abc39c4fb97d90e9276dd9b4073d793b4fdd7
Ruben Blazquez Garcia <rblazquezg@indra.es>
2020-02-04 17:11:48 +0100

    [DEMAD_15] Corrección de vulnerabilidades caja blanca
    Corrección de vulnerabilidades caja blanca

--- app/controllers/sandbox_controller.rb
+++ app/controllers/sandbox_controller.rb
@@ -13,20 +13,22 @@ class SandboxController < ApplicationController
   end
 
   def show
-    xxxx
+    
     if params[:template].index(".") # CVE-2014-0130
       render action: "index"
     elsif lookup_context.exists?("sandbox/#{params[:template]}")
       if params[:template] == "index"
         render action: "index"
       else
-        render :index
+        
+        render "sandbox/#{params[:template]}"
       end
 
     elsif lookup_context.exists?("sandbox/#{params[:template]}/index")
-      render :index
+      
+      render "sandbox/#{params[:template]}/index"
     else
-      render :index
+      render action: "index"
     end
   end
 
