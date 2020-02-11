3560e95a1ff6f0b3d9ae07d0858df557c8d4ea7e
Angel Perez <iAngel.p93@gmail.com>
2020-02-11 12:35:21 -0400

    Improve coverage for moderated texts feature spec

--- spec/features/admin/moderated_texts_spec.rb
+++ spec/features/admin/moderated_texts_spec.rb
@@ -2,6 +2,11 @@ require "rails_helper"
 
 feature "Moderated texts", type: :feature do
   let(:admin) { create(:administrator) }
+  let(:comment) { create(:comment,
+    body: "vulgar comment",
+    commentable: create(:debate),
+    user: admin.user)
+  }
 
   background do
     login_as(admin.user)
@@ -31,32 +36,26 @@ feature "Moderated texts", type: :feature do
 
   context "Edit" do
     let!(:word) { create(:moderated_text, text: "vulgar") }
-    let(:comment) { create(:comment,
-      body: "vulgar comment",
-      commentable: create(:debate),
-      user: admin.user)
-    }
-
     let!(:offense) { create(:moderated_content,
       moderable: comment,
       moderated_text: word)
     }
 
     scenario "Updates an existing word" do
-      another_word = create(:moderated_text)
+      another_word = create(:moderated_text, text: "nasty")
 
       visit admin_moderated_texts_path
-      expect(page).to have_content("bad word")
+      expect(page).to have_content("nasty")
 
       within "#moderated_text_#{another_word.id}" do
         click_link "Edit"
       end
 
-      fill_in "moderated_text_text", with: "disgusting word"
+      fill_in "moderated_text_text", with: "disgusting"
       click_button "Modify word"
 
-      expect(page).to have_content("disgusting word")
-      expect(page).not_to have_content("bad word")
+      expect(page).to have_content("disgusting")
+      expect(page).not_to have_content("nasty")
       expect(page).to have_content("Word modified successfully")
     end
 
@@ -72,18 +71,37 @@ feature "Moderated texts", type: :feature do
   end
 
   context "Delete" do
-    background do
-      bad_word = create(:moderated_text)
-    end
+    let!(:word) { create(:moderated_text, text: "vulgar") }
 
     scenario "Removes a moderated word", :js do
       visit admin_moderated_texts_path
-      expect(page).to have_content('bad word')
+      expect(page).to have_content('vulgar')
 
       accept_confirm { click_link 'Delete' }
 
       expect(page).to have_content('Word successfully deleted.')
-      expect(page).not_to have_content('bad word')
+      expect(page).not_to have_content('vulgar')
+    end
+
+    scenario "Removing a moderated word deletes all its associated offenses", :js do
+      create(:moderated_content, moderable: comment, moderated_text: word)
+
+      visit admin_moderated_texts_path
+
+      within "#moderated_text_#{word.id}" do
+        expect(page).to have_content("vulgar")
+        expect(page).to have_content("1")
+        accept_confirm { click_link 'Delete' }
+      end
+
+      expect(page).not_to have_content("vulgar")
+      expect(page).not_to have_content("1")
+
+      visit admin_auto_moderated_content_index_path
+
+      expect(page).not_to have_content("vulgar")
+      expect(page).not_to have_content("#{comment.body}")
+      expect(page).to have_content("No comments have been automatically moderated")
     end
   end
 end
