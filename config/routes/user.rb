07ec68d2769b76d60af3108df3455ee59a6d2729
Angel Perez <iAngel.p93@gmail.com>
2020-02-13 11:34:50 -0400

    Add 'Edit' link for offensive, non-moderated comments

--- config/routes/user.rb
+++ config/routes/user.rb
@@ -1,3 +1,7 @@
 resources :users, only: [:show] do
   resources :direct_messages, only: [:new, :create, :show]
+
+  member do
+    get '/comments/:comment_id/edit', to: "users#edit_comment", as: "edit_comment"
+  end
 end
