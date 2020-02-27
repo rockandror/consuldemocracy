8ff86ff2e1b7ce6c6e2cc7ec6320ebd7622bf8d7
taitus <sebastia.roig@gmail.com>
2020-02-27 17:53:57 +0100

    Improve display errors in ModeratedTextsController

--- app/controllers/admin/moderated_texts_controller.rb
+++ app/controllers/admin/moderated_texts_controller.rb
@@ -19,7 +19,6 @@ class Admin::ModeratedTextsController < Admin::BaseController
       notice = t("admin.moderated_texts.create.notice")
       redirect_to admin_moderated_texts_path, notice: notice
     else
-      flash.now[:error] = t("admin.moderated_texts.create.error")
       render :new
     end
   end
