24526ed5b26eadff5f6b058b5fabd15e471f9765
taitus <sebastia.roig@gmail.com>
2020-03-03 14:14:37 +0100

    Add specs for moderation emails
    Include comment body to moderation emails

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -14,4 +14,42 @@ describe Mailer do
       expect(email.subject).to include("comentado")
     end
   end
+
+  describe "#confirmed_moderation" do
+    let!(:comment) { create(:comment, body: "vulgar comment") }
+
+    before do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it "sends confirmed_moderation email" do
+      Mailer.confirmed_moderation(comment).deliver_now
+
+      email = open_last_email
+
+      expect(email).to deliver_from("CONSUL <noreply@consul.dev>")
+      expect(email).to deliver_to(comment.author)
+      expect(email).to have_subject I18n.t("mailers.moderable.confirmed_moderation_subject", moderable: I18n.t("mailers.moderable.resource.comment"))
+      expect(email).to have_body_text(comment.body)
+    end
+  end
+
+  describe "#declined_moderation" do
+    let!(:comment) { create(:comment, body: "vulgar comment") }
+
+    before do
+      ActionMailer::Base.deliveries.clear
+    end
+
+    it "sends declined_moderation email" do
+      Mailer.declined_moderation(comment).deliver_now
+
+      email = open_last_email
+
+      expect(email).to deliver_from("CONSUL <noreply@consul.dev>")
+      expect(email).to deliver_to(comment.author)
+      expect(email).to have_subject I18n.t("mailers.moderable.declined_moderation_subject", moderable: I18n.t("mailers.moderable.resource.comment"))
+      expect(email).to have_body_text(comment.body)
+    end
+  end
 end
