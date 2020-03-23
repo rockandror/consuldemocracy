d7871c753646e22af5c332832345980bcb587204
taitus <sebastia.roig@gmail.com>
2020-03-23 20:28:05 +0100

    Display filter "pending"  as default in Moderated Content
    To facilitate the work of the moderators we replaced the "all" filter with the
     "pending" filter. This way the moderators will have in the pending tab only
     the pending offenses to moderate.

--- app/controllers/admin/auto_moderated_content_controller.rb
+++ app/controllers/admin/auto_moderated_content_controller.rb
@@ -1,8 +1,8 @@
 class Admin::AutoModeratedContentController < Admin::BaseController
   before_action :load_contents, only: [:show_again, :confirm_moderation]
 
-  VALID_FILTERS = %w[confirmed declined].freeze
-  has_filters %w{all confirmed declined}, only: :index
+  VALID_FILTERS = %w[pending confirmed declined].freeze
+  has_filters %w{pending confirmed declined}, only: :index
 
   def index
     @moderated_comments = ::Comment.where(
@@ -13,13 +13,13 @@ class Admin::AutoModeratedContentController < Admin::BaseController
   def show_again
     @contents.update_all(declined_at: Time.current)
     Mailer.declined_moderation(@contents.first.moderable).deliver_later
-    redirect_to admin_auto_moderated_content_index_path
+    redirect_to admin_auto_moderated_content_index_path(filter: "pending")
   end
 
   def confirm_moderation
     @contents.update_all(confirmed_at: Time.current)
     Mailer.confirmed_moderation(@contents.first.moderable).deliver_later
-    redirect_to admin_auto_moderated_content_index_path
+    redirect_to admin_auto_moderated_content_index_path(filter: "pending")
   end
 
   private
@@ -27,7 +27,7 @@ class Admin::AutoModeratedContentController < Admin::BaseController
       if params[:filter].present? && is_valid_filter?
         ::ModeratedContent.send("#{params[:filter]}").pluck(:moderable_id)
       else
-        ::ModeratedContent.all.pluck(:moderable_id)
+        ::ModeratedContent.pending.pluck(:moderable_id)
       end
     end
 
