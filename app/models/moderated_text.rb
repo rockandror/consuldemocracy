53706b91b02829c2d430379a704b54c72426d0e9
taitus <sebastia.roig@gmail.com>
2020-03-23 20:29:59 +0100

    Remove Paranoia in ModeratedText

--- app/models/moderated_text.rb
+++ app/models/moderated_text.rb
@@ -1,9 +1,6 @@
 class ModeratedText < ApplicationRecord
   default_scope { order("text ASC") }
 
-  acts_as_paranoid column: :hidden_at
-  include ActsAsParanoidAliases
-
   has_many :comments, through: :moderated_contents
   has_many :moderated_contents, dependent: :destroy, foreign_key: "moderated_text_id"
 
