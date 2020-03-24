9b96d594564a3426236ef69526041e43f10d85c8
Laura Isael Blanco Calvo <liblanco@indra.es>
2020-03-24 14:07:14 +0100

    [DEMAD-34] Corrección error borrado de usuarios
    Correcion al eliminar un administrador

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,6 +1,6 @@
 class Administrator < ApplicationRecord
   belongs_to :user, touch: true
-  delegate :name, :email, :name_and_email, to: :user
+  delegate :name, :email, :name_and_email, to: :user, allow_nil: true
 
   validates :user_id, presence: true, uniqueness: true
 end
