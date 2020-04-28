ecf1193138b016b3264a0f3a08642f82ef2882c0
Javi Martín <javim@elretirao.net>
2020-04-28 15:32:22 +0200

    Remove tasks to upgrade to version 1.1
    These tasks are not needed for new installations, and in existing
    installations they've already been executed when upgrading to version
    1.1.
    
    One of them also raises a warning in Rails 5.2:
    
    DEPRECATION WARNING: Dangerous query method (method whose arguments are
    used as raw SQL) called with non-attribute argument(s): "MIN(id) as id".
    Non-attribute arguments will be disallowed in Rails 6.0. This method
    should not be called with user-provided values, such as request
    parameters or model attributes. Known-safe values can be passed by
    wrapping them in Arel.sql()

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,21 +73,12 @@ task :refresh_sitemap do
   end
 end
 
-desc "Create pid and socket folders needed by puma and convert unicorn sockets into symbolic links \
-      to the puma socket, so legacy nginx configurations pointing to the unicorn socket keep working"
+desc "Create pid and socket folders needed by puma"
 task :setup_puma do
   on roles(:app) do
     with rails_env: fetch(:rails_env) do
       execute "mkdir -p #{shared_path}/tmp/sockets; true"
       execute "mkdir -p #{shared_path}/tmp/pids; true"
-
-      if test("[ -e #{shared_path}/tmp/sockets/unicorn.sock ]")
-        execute "ln -sf #{shared_path}/tmp/sockets/puma.sock #{shared_path}/tmp/sockets/unicorn.sock; true"
-      end
-
-      if test("[ -e #{shared_path}/sockets/unicorn.sock ]")
-        execute "ln -sf #{shared_path}/tmp/sockets/puma.sock #{shared_path}/sockets/unicorn.sock; true"
-      end
     end
   end
 end
