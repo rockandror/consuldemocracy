c353438e28a4242b06032856d2df87319d328a61
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2020-04-30 09:05:16 +0200

    [DEMAD-58] Correcciones con pruebas
    Correcciones con pruebas

--- app/models/legislation/category.rb
+++ app/models/legislation/category.rb
@@ -1,6 +1,6 @@
 class Legislation::Category < ApplicationRecord
     belongs_to :process, class_name: "Legislation::Process", foreign_key: "legislation_process_id"
-    has_and_belongs_to_many :other_proposals, -> { order(:id) }, class_name: "Legislation::OtherProposal", foreign_key: "legislation_other_proposal_id", :join_table => :legislation_cat_prop
+    has_and_belongs_to_many :other_proposals, -> { order(:id) }, class_name: "Legislation::OtherProposal", :join_table => :legislation_cat_prop, dependent: :destroy
 
     validates :name, presence: true
 end
