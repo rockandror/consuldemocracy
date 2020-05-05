d7c5cd374a921f7cdd4a7016fb929f305524136b
Ruben Blazquez Garcia <rblazquezg@indra.es>
2020-05-05 17:01:21 +0200

    [DEMAD-26] Respuestas cerradas
    Respuestas cerradas

--- config/routes/legislation.rb
+++ config/routes/legislation.rb
@@ -11,7 +11,7 @@ namespace :legislation do
     end
 
     resources :questions, only: [:show] do
-      resources :answers, only: [:create,:update]
+      resources :answers, only: [:create]
     end
 
     resources :proposals do
