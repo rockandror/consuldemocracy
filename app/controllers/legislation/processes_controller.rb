fef30a078632806f368881938aacd77dd5c661f8
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2020-05-14 14:19:27 +0200

    [DEMAD-62] Corrección para entorno
    Corrección para entorno

--- app/controllers/legislation/processes_controller.rb
+++ app/controllers/legislation/processes_controller.rb
@@ -129,8 +129,9 @@ class Legislation::ProcessesController < Legislation::BaseController
     params[:filter] = "shops" if params[:filter].blank? && !params[:type].blank?
     
     if params[:type].blank?
-
-      if @current_filter == "random"
+      if !params[:filter].blank? 
+        @proposals = @proposals.joins(:categories).where("legislation_categories.tag = ?", params[:filter]).page(params[:page])
+      elsif @current_filter == "random"
         @proposals = @proposals.sort_by_random(session[:random_seed]).page(params[:page])
       elsif @current_filter == "winners"
         @proposals = @proposals.send(@current_filter).page(params[:page])
@@ -138,8 +139,6 @@ class Legislation::ProcessesController < Legislation::BaseController
         @proposals = Kaminari.paginate_array(@proposals.where("cached_votes_up > 0").sort_by {|x| x.likes}.reverse.take(10)).page(params[:page])
       elsif @current_filter == "updated"
         @proposals = @proposals.order(updated_at: :desc).page(params[:page])
-      elsif !params[:filter].blank? 
-        @proposals = @proposals.joins(:categories).where("legislation_categories.tag = ?", params[:filter]).page(params[:page])
       else
         @proposals = @proposals.order('id DESC').page(params[:page])
       end
