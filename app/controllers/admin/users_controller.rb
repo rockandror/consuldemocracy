d5f0daf5e65bb8af322bbcfe2c706b9c16bcacc3
Ruben Blazquez Garcia <rblazquezg@indra.es>
2020-05-25 16:34:41 +0200

    [DEMAD-63] Borrado de usuarios

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -123,6 +123,11 @@ class Admin::UsersController < Admin::BaseController
     if !params[:hidden_data].blank?
       user = User.find(params[:id])
       user.username = "Usuario dado de baja-" + user.id.to_s + "-" + params[:hidden_data].to_s
+      user.date_hide = Date.today
+      user.email = ""
+      user.document_number = ""
+      user.confirmed_phone = ""
+      user.gender = ""
       user.save
       redirect_to admin_users_path, notice: "Usuario #{user.id.to_s} dado de baja." 
     else
@@ -131,6 +136,6 @@ class Admin::UsersController < Admin::BaseController
   end
   
   def hide_params
-    params.require(:hide).permit(:hidden_data)
+    params.require(:hide).permit(:hidden_datak, :date_hide)
   end
 end
