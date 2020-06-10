66bae3198fc4abda8748c457e57f837c249296b0
taitus <sebastia.roig@gmail.com>
2020-06-11 01:03:00 +0200

    Refactor. Try remove 0..-2 range

--- config/initializers/routes_hierarchy.rb
+++ config/initializers/routes_hierarchy.rb
@@ -6,23 +6,17 @@ module ActionDispatch::Routing::UrlFor
   def resource_hierarchy_for(resource)
     resolve = resolve_for(resource)
 
-    if resolve
-      if resolve.last.is_a?(Hash)
-        [resolve.first, *resolve.last.values]
-      else
-        resolve
-      end
-    else
-      resource
-    end
+    return resource unless resolve
+    resolve.last.is_a?(Hash) ? [resolve.first, *resolve.last.values] : resolve
   end
 
   def admin_polymorphic_path(resource, options = {})
     if %w[Budget::Group Budget::Heading Poll::Booth Poll::Officer
           Poll::Question Poll::Question::Answer::Video].include?(resource.class.name)
       resolve = resolve_for(resource)
+      resolve_options = resolve.pop
 
-      polymorphic_path([:admin, *resolve[0..-2]], options.merge(resolve.last))
+      polymorphic_path([:admin, *resolve], options.merge(resolve_options))
     else
       polymorphic_path([:admin, *resource_hierarchy_for(resource)], options)
     end
