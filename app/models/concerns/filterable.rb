0bc047656e9cdcc7f7a85fe1c640007df01b21bb
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2020-09-09 18:47:18 +0200

    [DEMAD-92] Corrección de errores
    Corrección de errores

--- app/models/concerns/filterable.rb
+++ app/models/concerns/filterable.rb
@@ -6,10 +6,10 @@ module Filterable
     scope :by_date_range,     ->(date_range)     { where(created_at: date_range) }
   end
 
-  class_methods do
+  module ClassMethods
 
-    def filter(params)
-      resources = all
+    def filter(params, resources=nil)
+      resources = all if resources.blank?
       params.each do |filter, value|
         if allowed_filter?(filter, value)
           resources = resources.send("by_#{filter}", value)
