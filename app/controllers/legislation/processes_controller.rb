4cd7d414e878e6d2abdf0acf673324a262f94f19
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2020-11-24 13:35:13 +0100

    [DEMAD-101] Correccion de visibilidad
    Corrección visibilidad

--- app/controllers/legislation/processes_controller.rb
+++ app/controllers/legislation/processes_controller.rb
@@ -11,7 +11,7 @@ class Legislation::ProcessesController < Legislation::BaseController
   def index
     @current_filter ||= "open"
     @processes = ::Legislation::Process.send(@current_filter).includes(:geozones).published
-                 .not_in_draft.where(film_library: false).order(start_date: :desc).page(params[:page])
+                 .not_in_draft.order(start_date: :desc).page(params[:page])
   end
 
   def show
