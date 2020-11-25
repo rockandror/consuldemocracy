d1c6e3dfbd132f10667aef9e67d523e0adba8324
Javi Martín <javim@elretirao.net>
2020-11-25 15:39:16 +0100

    More

--- app/controllers/sdg_management/goals_controller.rb
+++ app/controllers/sdg_management/goals_controller.rb
@@ -3,6 +3,6 @@ class SDGManagement::GoalsController < SDGManagement::BaseController
   load_and_authorize_resource class: "SDG::Goal", only: [:index]
 
   def index
-    @goals = SDG::Goal.order(:code)
+    @goals = @goals.order(:code)
   end
 end
