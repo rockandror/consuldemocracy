441cb9c52aa1abbd6e0c1c3c5862899c2b20a5d6
Antonio Javier Moreno Hernández <ajmorenoh@indra.es>
2021-02-01 16:08:06 +0100

    [DEMAD-110] Desarrollo de funcionalidades front

--- app/controllers/sures_controller.rb
+++ app/controllers/sures_controller.rb
@@ -1,18 +1,61 @@
 class SuresController < SuresBaseController
-    #include FeatureFlags
-    #include FlagActions
-
-    #feature_flag :actuations
     
     def index
         @cards = Sures::CustomizeCard.body
     end
 
     def search
-        @actuations = Sures::Actuation.all.page(params[:page])
+        @actuations = []
+        @sures_searchs_settings = Sures::SearchSetting.search_settings.order(id: :asc)
+        @sures_orders_filter = Sures::SearchSetting.order_settings.order(id: :asc)
+       
+       
+        run_search(params)
     end
 
     def actuation
         @actuation = Sures::Actuation.find_by(id: params[:id])
+    end
+
+    private
+
+    def run_search(parametrize)
+        aux_active = false
+
+        aux_fields = sures_searchs_settings.map {|f| f.title.parameterize.underscore.to_s }
+        aux_fields.push('search')
+
+        aux_fields.each do |f|
+            if !parametrize[f.to_sym].blank?
+                aux_active = true
+                break
+            end
+        end
+
+        if aux_active
+            @actuations = Sures::Actuation.joins(:translations).all
+
+            
+
+
+
+            aux_order = "updated_at desc"
+            if  @sures_orders_filter.blank?
+                @type = ""
+            elsif !params[:type].blank?
+                aux = @sures_orders_filter.select {|order| order.title.parameterize.underscore == params[:type] }.first
+                aux_order = "#{aux.field} #{aux.rules.split(';').length > 1 ? aux.rules.split(';')[1] : aux.rules }"
+                @type = params[:type]
+            else
+                aux = @sures_orders_filter.select {|order| order.rules.to_s.include?('default') }.first
+                if aux.blank?
+                    aux = @sures_orders_filter.first
+                end
+                aux_order = "#{aux.field} #{aux.rules.split(';').length > 1 ? aux.rules.split(';')[1] : aux.rules }"
+                @type = aux.title.parameterize.underscore
+            end
+            @actuations = @actuations.order(aux_order)
+            @actuations = @actuations.page(params[:page])
+        end
     end
 end
