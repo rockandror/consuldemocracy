83bfcccc8209d1624c41ada9d71db090ec737abd
Michael <info@michaelnieuwenweg.nl>
2021-03-17 09:18:50 +0100

    working pvda2021

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,5 +1,18 @@
 #!/usr/bin/env puma
 
-_load_from File.expand_path("../defaults.rb", __FILE__)
+#_load_from File.expand_path("../defaults.rb", __FILE__)
 
 environment "production"
+
+# Puma can serve each request in a thread from an internal thread pool.
+# Default is set to 5 threads for minimum and maximum, matching the
+# default thread size of Active Record.
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }.to_i
+threads threads_count, threads_count
+
+port        ENV.fetch("PORT") { 3000 }
+#environment "development"
+
+# Allow puma to be restarted by `rails restart` command.
+plugin :tmp_restart
+
