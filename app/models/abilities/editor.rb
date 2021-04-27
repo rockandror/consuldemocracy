2bef628a37f201d6c98bdfc8da440acd05492a7a
Ruben Blazquez Garcia <rblazquezg@indra.es>
2021-04-27 13:50:52 +0200

    [DEMAD-111] Corrección permisos Editor

--- app/models/abilities/editor.rb
+++ app/models/abilities/editor.rb
@@ -6,6 +6,7 @@ module Abilities
       merge Abilities::Common.new(user)
       can :manage, Dashboard::Action
       can [:manage], Dashboard::AdministratorTask
+      can [:search, :edit, :update, :create, :index, :destroy], Banner
    
       can(:read_admin_stats, Budget) { |budget| budget.balloting_or_later? }
     end
