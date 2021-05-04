d4b16af79cd4eaa114a3306896a9a9fc1c6c92dc
Javi Martín <javim@elretirao.net>
2021-05-04 15:42:59 +0200

    Extract external links into application config
    We might use the Setting model in the end; for now we're configuring
    them with environment variables like we did before.

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,5 +1,12 @@
 module Consul
   class Application < Rails::Application
     config.i18n.available_locales = %w[es]
+
+    config.sites = {
+      open_government: ENV["LOGO_CANARIAS_GOVERNMENT_URL"] || "https://www.gobiernodecanarias.org/",
+      transparency: ENV["TRANSPARENCY_URL"] || "https://www.gobiernodecanarias.org/transparencia/",
+      open_data: ENV["OPEN_DATA_URL"] || "https://datos.canarias.es/",
+      citizen_participation: ENV["LOGO_CITIZEN_PARTICIPATION_URL"] || "https://www.gobiernodecanarias.org/participacionciudadana/"
+    }
   end
 end
