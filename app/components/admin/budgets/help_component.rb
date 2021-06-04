99933313a68b2f61e534b89967f805d8c828898b
taitus <sebastia.roig@gmail.com>
2021-06-04 19:15:23 +0200

    Adapt help component to multiple and single budget mode
    Pending review locales
    
    Co-Authored-By: decabeza <alberto@decabeza.es>

--- app/components/admin/budgets/help_component.rb
+++ app/components/admin/budgets/help_component.rb
@@ -1,13 +1,20 @@
 class Admin::Budgets::HelpComponent < ApplicationComponent
-  attr_reader :i18n_namespace
+  attr_reader :i18n_namespace, :mode
 
-  def initialize(i18n_namespace)
+  def initialize(i18n_namespace, mode: nil)
     @i18n_namespace = i18n_namespace
+    @mode = mode
   end
 
   private
 
     def text
-      t("admin.#{i18n_namespace}.index.help")
+      if i18n_namespace == "budgets"
+        t("admin.#{i18n_namespace}.index.help_block")
+      elsif mode == "single"
+        t("admin.#{i18n_namespace}.index.single.help_block")
+      else
+        t("admin.#{i18n_namespace}.index.multiple.help_block")
+      end
     end
 end
