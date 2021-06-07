Ajuntament-de-Palma/consul

Render link to budget header
- Allow to define a link (text and url) on budget form for render on the budget
header.
- Improve styles

db/migrate/20200213163214_add_main_link_to_budgets.rb
@@ -1,0 +1,6 @@
+class AddMainLinkToBudgets < ActiveRecord::Migration[5.2]
+  def change
+    add_column :budgets, :main_link_text, :string
+    add_column :budgets, :main_link_url, :string
+  end
+end

rockandror/consuldemocracy

Co-authored-by: Senén Rodero Rodríguez <senenrodero@gmail.com>

db/migrate/20200213163214_add_main_link_to_budgets.rb
@@ -1,0 +1,6 @@
+class AddMainLinkToBudgets < ActiveRecord::Migration[5.2]
+  def change
+    add_column :budgets, :main_link_url, :string
+    add_column :budget_translations, :main_link_text, :string
+  end
+end