355722aa0d509d9fc864d9ee28ac7b3946416b23
Senén Rodero Rodríguez <senenrodero@gmail.com>
2021-07-19 12:52:03 +0200

    Do not load custom settings on the test environment
    As it would break some tests.
    
    This way custom settings will apply to all other environments.

--- app/models/custom/setting.rb
+++ app/models/custom/setting.rb
@@ -1,38 +1,40 @@
 require_dependency Rails.root.join("app", "models", "setting").to_s
 
-class Setting
-  class << self
-    alias_method :consul_defaults, :defaults
+unless Rails.env.test?
+  class Setting
+    class << self
+      alias_method :consul_defaults, :defaults
 
-    def defaults
-      consul_defaults.merge({
-        "org_name": "PvdA",
-        "url": "https://pvda.nl",
-        "proposal_code_prefix": "MOTIE",
-        "max_votes_for_proposal_edit": 1,
-        "facebook_handle": nil,
-        "instagram_handle": nil,
-        "telegram_handle": false,
-        "twitter_handle": nil,
-        "twitter_hashtag": nil,
-        "youtube_handle": nil,
-        "feature.wordpress_login": true,
-        "feature.facebook_login": false,
-        "feature.google_login": false,
-        "feature.twitter_login": false,
-        "feature.signature_sheets": false,
-        "feature.community": false,
-        "feature.map": false,
-        "feature.remote_census": false,
-        "feature.remote_translations": false,
-        "feature.translation_interface": false,
-        "votes_for_proposal_success": 100,
-        "months_to_archive_proposals": 1,
-        "mailer_from_address": "noreply@pvda.nl",
-        "featured_proposals_number": 3,
-        "process.polls": false,
-        "process.budgets": false,
-      })
+      def defaults
+        consul_defaults.merge({
+          "org_name": "PvdA",
+          "url": "https://pvda.nl",
+          "proposal_code_prefix": "MOTIE",
+          "max_votes_for_proposal_edit": 1,
+          "facebook_handle": nil,
+          "instagram_handle": nil,
+          "telegram_handle": false,
+          "twitter_handle": nil,
+          "twitter_hashtag": nil,
+          "youtube_handle": nil,
+          "feature.wordpress_login": true,
+          "feature.facebook_login": false,
+          "feature.google_login": false,
+          "feature.twitter_login": false,
+          "feature.signature_sheets": false,
+          "feature.community": false,
+          "feature.map": false,
+          "feature.remote_census": false,
+          "feature.remote_translations": false,
+          "feature.translation_interface": false,
+          "votes_for_proposal_success": 100,
+          "months_to_archive_proposals": 1,
+          "mailer_from_address": "noreply@pvda.nl",
+          "featured_proposals_number": 3,
+          "process.polls": false,
+          "process.budgets": false,
+        })
+      end
     end
   end
 end
