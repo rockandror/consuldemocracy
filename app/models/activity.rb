f4aa6578c20cc95bdb5edaafdce0e30457bc601d
taitus <sebastia.roig@gmail.com>
2021-07-23 13:04:44 +0200

    Register activity for new actions avoid/allow voting

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,8 @@ class Activity < ApplicationRecord
   belongs_to :actionable, -> { with_hidden }, polymorphic: true
   belongs_to :user, -> { with_hidden }, inverse_of: :activities
 
-  VALID_ACTIONS = %w[hide block restore valuate email].freeze
+  VALID_ACTIONS = %w[hide block restore valuate email disable_voting enable_voting
+    remove_voting_review].freeze
 
   validates :action, inclusion: { in: VALID_ACTIONS }
 
