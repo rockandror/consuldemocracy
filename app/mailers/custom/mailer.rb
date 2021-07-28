0e1a238ca21507b45f5c4f6aa9c30aca601dc351
taitus <sebastia.roig@gmail.com>
2021-07-28 10:54:12 +0200

    Send 'voting review' email to author after create a proposal

---
+++ app/mailers/custom/mailer.rb
@@ -0,0 +1,13 @@
+require_dependency Rails.root.join("app", "mailers", "mailer").to_s
+
+class Mailer
+  def voting_review(proposal)
+    @proposal = proposal
+    @email_to = @proposal.author.email
+    @author = @proposal.author
+
+    with_user(@author) do
+      mail(to: @email_to, subject: t("mailers.proposal.voting_review.subject"))
+    end
+  end
+end
