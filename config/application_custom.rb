34185de43548b934e7493c8447aad794d5eccbe8
taitus <sebastia.roig@gmail.com>
2021-08-04 17:36:11 +0200

    TMP: Add :en as available locale

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,7 +1,7 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :nl
-    config.i18n.available_locales = [:nl]
+    config.i18n.available_locales = [:en, :nl]
     config.time_zone = "Amsterdam"
 
     config.autoload_paths << "#{Rails.root}/app/mailers/custom"
