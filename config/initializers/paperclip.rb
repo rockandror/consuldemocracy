72f620af971e11e53f9351a58155b4a241f626c1
Senén Rodero Rodríguez <senenrodero@gmail.com>
2021-09-15 19:05:19 +0200

    Install `aws-sdk-s3` and setup Paperclip
    To connect with a AWS S3 bucket in place of using disk storage.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,15 @@
+if Rails.env.production? || Rails.env.staging? || Rails.env.preproduction?
+  Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:s3_host_name] = Rails.application.secrets.aws_s3_host_name
+  Paperclip::Attachment.default_options[:s3_protocol] = :https
+  Paperclip::Attachment.default_options[:s3_credentials] = {
+    access_key_id: Rails.application.secrets.aws_s3_access_key_id,
+    secret_access_key: Rails.application.secrets.aws_s3_secret_access_key,
+    s3_region: Rails.application.secrets.aws_s3_region
+  }
+  Paperclip::Attachment.default_options[:bucket] = Rails.application.secrets.aws_s3_bucket
+end
+
 class Paperclip::UrlGenerator
   private
 
