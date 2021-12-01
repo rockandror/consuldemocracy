9b1eef4f1d021659d87ea7fa17c4a40b1e92103d
Senén Rodero Rodríguez <senenrodero@gmail.com>
2021-12-01 15:42:09 +0100

    Use default session store implementation for secure option
    By default the implementation is:
    
    `secure: request.ssl?`
    
    which is exactly what we need.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,5 +2,4 @@
 
 Rails.application.config.session_store :cookie_store,
                                         key: ENV["COOKIES_NAME"] || "_consul_session",
-                                        secure: Rails.env.production?,
                                         path: ENV["CONSUL_RELATIVE_URL"] || "/"
