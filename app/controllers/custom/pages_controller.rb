ceefcc03e634af9b05d6c42b4b630a7c4fce3c21
Diego Calvo <diego.calvo@enreda.coop>
2021-12-22 09:17:03 +0100

    news pagination

--- app/controllers/custom/pages_controller.rb
+++ app/controllers/custom/pages_controller.rb
@@ -32,7 +32,7 @@ class PagesController < ApplicationController
 
   def news
     @menu_title = 'Cabildo Abierto'
-    @news = SiteCustomization::Page.where(is_news: true)
+    @news = SiteCustomization::Page.where(is_news: true).page(params[:page]).per(5)
     @related_pages = SiteCustomization::Page.where("slug LIKE :family", family: "\_cabildo\_%")
     render action: :news
   end
