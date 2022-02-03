e003f0d9af8983c283ab80198b0d6db62d1e9932
Javi Martín <javim@elretirao.net>
2022-02-03 16:55:11 +0100

    Use google's servers in the recaptcha service
    The staging servers don't allow access to recaptcha.net.

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,8 @@
 Recaptcha.configure do |config|
   config.site_key  = Rails.application.secrets.recaptcha_site_key
   config.secret_key = Rails.application.secrets.recaptcha_secret_key
+  config.api_server_url = "https://www.google.com/recaptcha/api.js"
+  config.verify_url = "https://www.google.com/recaptcha/api/siteverify"
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 
