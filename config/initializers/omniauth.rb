0b8e11763a98d911ba5f67473e2ba89f2d6a9b35
taitus <sebastia.roig@gmail.com>
2022-06-30 10:06:24 +0200

    Try see OmniAuth messages in the log v2 (Fixup to previous commit)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,1 +1,9 @@
-OmniAuth.config.failure_raise_out_environments = ["development", "staging"]
+OmniAuth.configure do |config|
+  # Always redirect to the failure endpoint if there is an error. Normally the
+  # exception would just be raised in development mode. This is useful for
+  # testing your Realme error handling in development.
+  config.failure_raise_out_environments = ["development", "staging"]
+
+  # We want to see OmniAuth messages in the log
+  config.logger = Rails.logger
+end
