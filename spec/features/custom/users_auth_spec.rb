f4bb9785a07d5a5060406cb7e4fe251db869d547
taitus <sebastia.roig@gmail.com>
2022-07-11 13:06:44 +0200

    Add omniauth saml section for sign in and sign up page

--- spec/features/custom/users_auth_spec.rb
+++ spec/features/custom/users_auth_spec.rb
@@ -11,13 +11,72 @@ feature "Users", :js do
         Setting["feature.saml_login"] = false
       end
 
+      scenario "Sign up with a confirmed email" do
+        saml_hash = {
+          provider: "saml",
+          uid: "ext-tester",
+          info: {
+            name: "samltester",
+            email: "tester@consul.dev",
+            verified: "1"
+          }
+        }
+        OmniAuth.config.add_mock(:saml, saml_hash)
+
+        visit new_user_registration_path
+        click_link "Access through CAS"
+
+        expect(page).to have_content "Successfully identified as Saml"
+        expect_to_be_signed_in
+
+        within("#notice") { find("button").click }
+        click_link "My account"
+        expect(page).to have_field "Username", with: "samltester"
+
+        click_link "Change my credentials"
+        expect(page).to have_field "Email", with: "tester@consul.dev"
+      end
+
+      scenario "Sign up with an unconfirmed email" do
+        saml_hash = {
+          provider: "saml",
+          uid: "ext-tester",
+          info: {
+            name: "samltester",
+            email: "tester@consul.dev"
+          }
+        }
+        OmniAuth.config.add_mock(:saml, saml_hash)
+
+        visit new_user_registration_path
+        click_link "Access through CAS"
+
+        expect(page).to have_content "To continue, please click on the confirmation link that we have sent you via email"
+
+        confirm_email
+        expect(page).to have_content "Your account has been confirmed"
+        expect(page).to have_current_path new_user_session_path
+
+        click_link "Access through CAS"
+
+        expect(page).to have_content "Successfully identified as Saml"
+        expect_to_be_signed_in
+
+        within("#notice") { find("button").click }
+        click_link "My account"
+        expect(page).to have_field "Username", with: "samltester"
+
+        click_link "Change my credentials"
+        expect(page).to have_field "Email", with: "tester@consul.dev"
+      end
+
       scenario "Sign in with a user with a SAML identity" do
         user = create(:user, username: "samltester", email: "tester@consul.dev", password: "My123456")
         create(:identity, uid: "ext-tester", provider: "saml", user: user)
         OmniAuth.config.add_mock(:saml, { provider: "saml", uid: "ext-tester" })
 
         visit new_user_session_path
-        click_link "Access to the Citizen Participation Platform"
+        click_link "Access through CAS"
 
         expect(page).to have_content "Successfully identified as Saml"
         expect_to_be_signed_in
@@ -44,7 +103,7 @@ feature "Users", :js do
         OmniAuth.config.add_mock(:saml, saml_hash)
 
         visit new_user_session_path
-        click_link "Access to the Citizen Participation Platform"
+        click_link "Access through CAS"
 
         expect(page).to have_content "Successfully identified as Saml"
         within("#notice") { find("button").click }
@@ -53,6 +112,26 @@ feature "Users", :js do
 
         click_link "Change my credentials"
         expect(page).to have_field "Email", with: "tester@consul.dev"
+      end
+
+      describe "Include SAML section" do
+        scenario "on sign in page" do
+          visit root_path
+
+          click_link "Register"
+
+          expect(page).to have_content "Only if you are staff of the Government of the Canary Islands:"
+          expect(page).to have_link "Access through CAS"
+        end
+
+        scenario "on sign up page" do
+          visit root_path
+
+          click_link "Sign in"
+
+          expect(page).to have_content "Only if you are staff of the Government of the Canary Islands:"
+          expect(page).to have_link "Access through CAS"
+        end
       end
     end
   end
