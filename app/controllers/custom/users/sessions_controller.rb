7edbd2454c0cf10a42a73cdbbc732a9e5c8ce6df
taitus <sebastia.roig@gmail.com>
2022-07-15 08:39:16 +0200

    Unify response time on authentication

--- app/controllers/custom/users/sessions_controller.rb
+++ app/controllers/custom/users/sessions_controller.rb
@@ -6,6 +6,9 @@ class Users::SessionsController
   prepend_before_action :check_captcha, only: [:create],
                                         if: -> { show_recaptcha_for?(params[:user][:login]) }
 
+  before_action :set_current_time
+  after_action :unify_response_time, only: [:create, :new]
+
   def show_recaptcha
     render json: { recaptcha: show_recaptcha_for?(params[:login]) }
   end
@@ -16,5 +19,13 @@ class Users::SessionsController
       user = User.find_by_username(login) || User.find_by_email(login)
       return false unless user.present?
       user.exceeded_failed_login_attempts?
+    end
+
+    def set_current_time
+      @@current_time = Time.now
+    end
+
+    def unify_response_time
+      sleep(1) until Time.now > @@current_time + 5.seconds
     end
 end
