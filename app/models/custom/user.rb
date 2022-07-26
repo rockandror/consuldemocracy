617557ac708a12d183ee8ca4b5686dc280b4a724
taitus <sebastia.roig@gmail.com>
2022-07-26 09:30:06 +0200

    Force password with complexity required
    Since commit 0b2bb35, we need all passwords must contain at least one digit
    and at least one upper-case letter.
    
    We were having some flakies because of the Devise.friendly_token that we use to
    generate passwords sometimes it generated passwords that did not valid
    because they did not contain any digit.
    
    We updated the code to always add a random digit between 0 and 9, in a random
    position of the generated token.
    
    We have modified this line:
    --  Devise.friendly_token[0, 20]
    ++ Devise.friendly_token.insert(rand(1..20), rand(0..9).to_s)
    
    In Devise friendly_token method by default, length is 20 characters, so we can
    avoid passing the parameter

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -30,4 +30,20 @@ class User
       manager&.destroy!
     end
   end
+
+  # Get the existing user by email if the provider gives us a verified email.
+  def self.first_or_initialize_for_oauth(auth)
+    oauth_email           = auth.info.email
+    oauth_email_confirmed = oauth_email.present? && (auth.info.verified || auth.info.verified_email)
+    oauth_user            = User.find_by(email: oauth_email) if oauth_email_confirmed
+
+    oauth_user || User.new(
+      username:  auth.info.name || auth.uid,
+      email: oauth_email,
+      oauth_email: oauth_email,
+      password: Devise.friendly_token.insert(rand(1..20), rand(0..9).to_s),
+      terms_of_service: '1',
+      confirmed_at: oauth_email_confirmed ? DateTime.current : nil
+    )
+  end
 end
