e15370aa8735d1edd88550ee7588e8babf80a637
taitus <sebastia.roig@gmail.com>
2022-07-26 15:49:21 +0200

    Force password with required complexity
    Since commit 0b2bb35, we need all passwords must contain at least one digit
    and at least one upper-case letter.
    
    Some tests were failing sometimes because of the Devise.friendly_token that we
    use to generate passwords sometimes generated passwords that were not valid
    because they did not contain any digits.
    
    We updated the code to make sure the password always contains a digit,
    lowercase and uppercase character.

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -3,6 +3,18 @@ require_dependency Rails.root.join('app', 'models', 'user').to_s
 class User
   devise :lockable, :session_limitable
 
+  class << self
+    alias_method :consul_first_or_initialize_for_oauth, :first_or_initialize_for_oauth
+
+    def first_or_initialize_for_oauth(auth)
+      consul_first_or_initialize_for_oauth(auth).tap do |user|
+        if user.password.present?
+          user.password += rand(0..9).to_s + ("a".."z").to_a.sample + ("A".."Z").to_a.sample
+        end
+      end
+    end
+  end
+
   def exceeded_failed_login_attempts?
     failed_attempts >= 1
   end
