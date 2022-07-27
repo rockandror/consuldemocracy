d5962fe8195d5a9dea8da7a27ece502c07ff6583
taitus <sebastia.roig@gmail.com>
2022-07-27 15:05:10 +0200

    Always render captcha on sign in
    Remove specific code to only render captcha  after consecutive failed attemps.

--- app/models/custom/user.rb
+++ app/models/custom/user.rb
@@ -1,9 +1,5 @@
 require_dependency Rails.root.join('app', 'models', 'user').to_s
 
 class User
-  devise :lockable, :session_limitable
-
-  def exceeded_failed_login_attempts?
-    failed_attempts >= 1
-  end
+  devise :session_limitable
 end
--- spec/features/users_auth_spec.rb
+++ spec/features/users_auth_spec.rb
@@ -101,48 +101,6 @@ feature 'Users' do
         expect(page).to have_link 'My activity', href: user_path(u2)
       end
 
-      scenario "resets failed attempts after successful login", :js do
-        user = create(:user, email: "manuela@consul.dev",
-                             password: "Judgement1234",
-                             failed_attempts: 3)
-        visit user_session_path
-
-        fill_in "Email or username", with: "manuela@consul.dev"
-        fill_in "Password", with: "Judgement1234"
-
-        expect(page).to have_css ".recaptcha"
-
-        click_button "Enter"
-
-        expect(page).to have_content "You have been signed in successfully."
-
-        logout
-        visit user_session_path
-        fill_in "Email or username", with: "manuela@consul.dev"
-        fill_in "Password", with: "Judgement1234"
-
-        expect(page).not_to have_css ".recaptcha"
-      end
-
-      scenario "shows recaptcha only after consecutive failed attempts", :js do
-        create(:user, email: "manuela@consul.dev", password: "Judgement1234", failed_attempts: 0)
-
-        visit new_user_session_path
-
-        fill_in "Email or username", with: "manuela@consul.dev"
-        fill_in "Password", with: "wrong_password"
-
-        expect(page).not_to have_css ".recaptcha"
-
-        click_button "Enter"
-        expect(page).to have_content "Invalid Email or username or password."
-
-        fill_in "Email or username", with: "manuela@consul.dev"
-        fill_in "Password", with: "wrong_password"
-
-        expect(page).to have_css ".recaptcha"
-      end
-
       scenario "unchecked recaptcha with correct login details", :js do
         allow_any_instance_of(Users::SessionsController).to receive(:verify_recaptcha).and_return false
         create(:user, email: "manuela@consul.dev", password: "Judgement1234", failed_attempts: 1)
