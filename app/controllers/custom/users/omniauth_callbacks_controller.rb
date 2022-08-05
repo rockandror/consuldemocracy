11cd36c9b4aa3294e5572e033b754c7cd8abf2a2
Javi Martín <javim@elretirao.net>
2022-08-05 13:49:20 +0200

    Avoid InvalidAuthenticationToken when SAML fails

--- app/controllers/custom/users/omniauth_callbacks_controller.rb
+++ app/controllers/custom/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 require_dependency Rails.root.join("app", "controllers", "users", "omniauth_callbacks_controller").to_s
 
 class Users::OmniauthCallbacksController
-  skip_before_action :verify_authenticity_token, only: :saml
+  skip_before_action :verify_authenticity_token, only: [:saml, :failure]
 
   def saml
     sign_in_with :saml_login, :saml
