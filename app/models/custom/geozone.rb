783d7f4bad79d5d1bbcd3b57c7f10fe71546afb3
Javi Martín <javim@elretirao.net>
2023-01-16 23:15:33 +0100

    Fix crash deleting a geozone with no associations
    The application was crashing since we added the relationship with audits
    in commit d728bd6f6 because audits don't belong to a geozone but to an
    auditable. The tests "Delete geozone with no associated elements" and
    "#safe_to_destroy? is true when not linked to other models" were failing
    because of this.

---
+++ app/models/custom/geozone.rb
@@ -0,0 +1,11 @@
+require_dependency Rails.root.join("app", "models", "geozone").to_s
+
+class Geozone
+  def safe_to_destroy?
+    Geozone.reflect_on_all_associations(:has_many).reject do |association|
+      association.name == :audits
+    end.all? do |association|
+      association.klass.where(geozone: self).empty?
+    end
+  end
+end
