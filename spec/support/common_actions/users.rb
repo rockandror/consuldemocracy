0e6a2c832d6d56ff9581c9139d4a95b152be4b17
Javi Martín <javim@elretirao.net>
2023-01-16 23:09:40 +0100

    Update "My account" link in users auth tests
    This link was moved to a different place in commit 76a45d7c6.

--- spec/support/common_actions/users.rb
+++ spec/support/common_actions/users.rb
@@ -75,11 +75,11 @@ module Users
   end
 
   def expect_to_be_signed_in
-    expect(find(".top-bar-right")).to have_content "My account"
+    expect(find(".top-links")).to have_content "My account"
   end
 
   def expect_not_to_be_signed_in
-    expect(find(".top-bar-right")).not_to have_content "My account"
+    expect(find(".top-links")).not_to have_content "My account"
   end
 
   def common_do_login_for(user, management:)
