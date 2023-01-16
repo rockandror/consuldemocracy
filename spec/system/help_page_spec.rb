e7d0f8fd6dec356eef54aace566dbc30d70df523
Javi Martín <javim@elretirao.net>
2023-01-16 23:28:51 +0100

    Simplify navigation in specs testing the help page

--- spec/system/help_page_spec.rb
+++ spec/system/help_page_spec.rb
@@ -38,10 +38,7 @@ describe "Help page" do
     Setting["feature.help_page"] = true
     Setting["feature.sdg"] = true
 
-    visit root_path
-    within("#navigation_bar") do
-      click_link "Help"
-    end
+    visit help_path
 
     expect(page).to have_link "Sustainable Development Goals help", href: sdg_help_path
   end
@@ -49,10 +46,7 @@ describe "Help page" do
   scenario "does not render the SDG help page link when the feature is disabled" do
     Setting["feature.sdg"] = nil
 
-    visit root_path
-    within("#navigation_bar") do
-      click_link "Help"
-    end
+    visit help_path
 
     expect(page).not_to have_link "Sustainable Development Goals help"
   end
