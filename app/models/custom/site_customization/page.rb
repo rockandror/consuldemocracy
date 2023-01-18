9d21878989f5f6bb59aabbcf2eef3f88ab113312
taitus <sebastia.roig@gmail.com>
2023-01-18 11:15:53 +0100

    Remove duplication in the models/custom folder
    The files in the custom folder should only contain the changes to the
    original file.

--- app/models/custom/site_customization/page.rb
+++ app/models/custom/site_customization/page.rb
@@ -1,26 +1,9 @@
-class SiteCustomization::Page < ApplicationRecord
-  VALID_STATUSES = %w[draft published].freeze
-  include Cardable
-  translates :title,       touch: true
-  translates :subtitle,    touch: true
-  translates :content,     touch: true
-  include Globalizable
+require_dependency Rails.root.join("app", "models", "site_customization", "page").to_s
 
+class SiteCustomization::Page
   audited on: [:create, :update, :destroy]
 
-  validates_translation :title, presence: true
-  validates :slug, presence: true,
-                   uniqueness: { case_sensitive: false },
-                   format: { with: /\A[0-9a-zA-Z\-_]*\Z/, message: :slug_format }
-  validates :status, presence: true, inclusion: { in: VALID_STATUSES }
   validates :news_date, presence: true, if: :is_news?
-
-  scope :published, -> { where(status: "published").sort_desc }
-  scope :sort_asc, -> { order("id ASC") }
-  scope :sort_desc, -> { order("id DESC") }
-  scope :with_more_info_flag, -> { where(status: "published", more_info_flag: true).sort_asc }
-  scope :with_same_locale, -> { joins(:translations).locale }
-  scope :locale, -> { where("site_customization_page_translations.locale": I18n.locale) }
 
   def self.search(terms)
     results = SiteCustomization::Page.joins(:translations).locale
@@ -45,10 +28,6 @@ class SiteCustomization::Page < ApplicationRecord
     else
       search(terms)
     end
-  end
-
-  def url
-    "/#{slug}"
   end
 
   def is_news?
