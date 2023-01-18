0e29ff30fca937cabbae86731e2039ad0f137894
taitus <sebastia.roig@gmail.com>
2023-01-18 16:45:40 +0100

    Move custom mailer method to custom file

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -6,5 +6,6 @@ module Consul
     config.i18n.available_locales = ['es']
 
     config.autoload_paths << "#{Rails.root}/app/controllers/custom/concerns"
+    config.autoload_paths << "#{Rails.root}/app/mailers/custom"
   end
 end
