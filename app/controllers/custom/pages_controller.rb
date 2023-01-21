df1d09d20811d9191851e9891f0a7a667e4f0e8c
Javi Martín <javim@elretirao.net>
2023-01-21 22:30:02 +0100

    Reuse original logic in custom pages controller
    This way we won't have to update the custom controller when we upgrade
    to Consul 1.4.1.

--- app/controllers/custom/pages_controller.rb
+++ app/controllers/custom/pages_controller.rb
@@ -1,8 +1,9 @@
 require_dependency Rails.root.join("app", "controllers", "pages_controller").to_s
 
 class PagesController
+  alias_method :consul_show, :show
+
   def show
-    @custom_page = SiteCustomization::Page.published.find_by(slug: params[:id])
     string_array = params[:id].split("_")
     family = string_array[1]
     @related_pages = SiteCustomization::Page.published.where("slug LIKE :family", family: "\_#{family}\_%")
@@ -18,14 +19,7 @@ class PagesController
       @menu_title = false
     end
 
-    if @custom_page.present?
-      @cards = @custom_page.cards
-      render action: :custom_page
-    else
-      render action: params[:id]
-    end
-  rescue ActionView::MissingTemplate
-    head 404, content_type: "text/html"
+    consul_show
   end
 
   def news
