e03a46516af5588660d3196f413ebd9609fa90eb
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-02-02 17:03:34 +0100

    Remove uneeded instance variables
    In the mail view the `subject` and the `email_to` instance
    variables are not used. However, the email_to needs to be an
    instance variable so the method `prevent_delivery_to_users_without_email`
    does not avoid the email delivery.

--- app/mailers/custom/mailer.rb
+++ app/mailers/custom/mailer.rb
@@ -3,12 +3,11 @@ require_dependency Rails.root.join("app", "mailers", "mailer").to_s
 class Mailer
   def contact(subject, intro, message)
     @message = message
-    @subject = subject
     @intro = intro
     @email_to = Rails.application.secrets.contact_email
 
     I18n.with_locale(I18n.default_locale) do
-      mail(to: @email_to, subject: @subject)
+      mail(to: @email_to, subject: subject)
     end
   end
 end
