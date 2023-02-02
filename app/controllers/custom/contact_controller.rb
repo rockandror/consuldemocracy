0e7491441da20d5c90e954350eff44165c3fc67e
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-02-02 14:20:06 +0100

    Use the new action from custom contact_controller to show the contact form
    We were using the pages controller to show this page.
    
    More things:
    * Move translations to the custom activemodel translations file
    * Do not use the required attribute in form fields
    * Do not render the * at the end of labels
    * Show validation errors

--- app/controllers/custom/contact_controller.rb
+++ app/controllers/custom/contact_controller.rb
@@ -1,24 +1,33 @@
 class ContactController < ApplicationController
   skip_authorization_check
 
-  invisible_captcha only: [:create], honeypot: :address
+  invisible_captcha only: [:create], honeypot: :address, scope: :contact_form
 
   def new
-    render action: :contact
+    @contact_form = ContactForm.new
   end
 
   def create
-    Mailer.contact(contact_params[:subject], build_intro, contact_params[:email_body]).deliver_later
-    redirect_to root_path, notice: t("contact.success")
+    @contact_form = ContactForm.new(contact_params)
+    if @contact_form.valid?
+      Mailer.contact(contact_params[:subject], build_intro, contact_params[:message]).deliver_later
+      redirect_to root_path, notice: t("contact.success")
+    else
+      render :new
+    end
   end
 
   private
 
     def contact_params
-      params.permit(:username, :email, :subject, :email_body)
+      params.require(:contact_form).permit(allowed_params)
+    end
+
+    def allowed_params
+      [:name, :email, :subject, :message, :terms_of_service]
     end
 
     def build_intro
-      t("mailers.contact.intro", name: contact_params[:username], email: contact_params[:email], message: contact_params[:email_body])
+      t("mailers.contact.intro", name: contact_params[:name], email: contact_params[:email], message: contact_params[:message])
     end
 end
--- spec/system/custom/contact_spec.rb
+++ spec/system/custom/contact_spec.rb
@@ -6,10 +6,10 @@ describe "Contact form" do
   scenario "Sends email to cabildo and redirects to home page when form is fullfilled correctly" do
     visit contact_path(locale: "es")
 
-    fill_in "Nombre *", with: "Fulanito"
-    fill_in "Email *", with: "fulanito@example.org"
-    fill_in "Asunto *", with: "Petición de soporte"
-    fill_in "Mensaje *", with: "Necesito ayuda ..."
+    fill_in "Nombre", with: "Fulanito"
+    fill_in "Email", with: "fulanito@example.org"
+    fill_in "Asunto", with: "Petición de soporte"
+    fill_in "Mensaje", with: "Necesito ayuda ..."
     check "Acepto la Política de Privacidad"
     click_on "Enviar"
 
@@ -21,10 +21,10 @@ describe "Contact form" do
   scenario "Create with invisible_captcha honeypot field", :no_js do
     visit contact_path(locale: "es")
 
-    fill_in "Nombre *", with: "Fulanito"
-    fill_in "Email *", with: "fulanito@example.org"
-    fill_in "Asunto *", with: "Petición de soporte"
-    fill_in "Mensaje *", with: "Necesito ayuda ..."
+    fill_in "Nombre", with: "Fulanito"
+    fill_in "Email", with: "fulanito@example.org"
+    fill_in "Asunto", with: "Petición de soporte"
+    fill_in "Mensaje", with: "Necesito ayuda ..."
     check "Acepto la Política de Privacidad"
     fill_in "Si eres humano, por favor ignora este campo", with: "This is a honeypot field"
     click_on "Enviar"
@@ -37,13 +37,22 @@ describe "Contact form" do
   scenario "Do not send the email when user does not accept the privacy policy" do
     visit contact_path(locale: "es")
 
-    fill_in "Nombre *", with: "Fulanito"
-    fill_in "Email *", with: "fulanito@example.org"
-    fill_in "Asunto *", with: "Petición de soporte"
-    fill_in "Mensaje *", with: "Necesito ayuda ..."
+    fill_in "Nombre", with: "Fulanito"
+    fill_in "Email", with: "fulanito@example.org"
+    fill_in "Asunto", with: "Petición de soporte"
+    fill_in "Mensaje", with: "Necesito ayuda ..."
     click_on "Enviar"
 
     expect(page).to have_content("debe ser aceptado")
     expect(page).not_to have_content "Mensaje enviado correctamente"
+  end
+
+  scenario "Shows validation errors when validation fails" do
+    visit contact_path(locale: "es")
+
+    click_on "Enviar"
+
+    expect(page).to have_content("no puede estar en blanco", count: 4)
+    expect(page).to have_content("debe ser aceptado", count: 1)
   end
 end
