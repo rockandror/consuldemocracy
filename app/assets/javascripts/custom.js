ed3d9ad2ff6ea9240d2fda69c978995e0c431c2e
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-02-02 10:26:36 +0100

    Place script to track user cookies consent in the standard directory
    Run the script only when the `google_tag_manager_id` is
    defined. This way we avoid calling this script in development,
    tests and preproduction environments.

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -6,11 +6,13 @@
 //
 //= require cookies_eu
 //= require google_search
+//= require consent_banner
 
 var initialize_modules = function() {
   "use strict";
 
   App.GoogleSearch.initialize();
+  App.ConsentBanner.initialize();
 };
 
 $(document).on("turbolinks:load", initialize_modules);
