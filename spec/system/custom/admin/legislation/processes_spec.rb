354910e18857ecb81aa0cddd8eac9286e375fd47
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-02-03 16:56:04 +0100

    Add legislation process id to filename
    So it generates a different filename per process.

--- spec/system/custom/admin/legislation/processes_spec.rb
+++ spec/system/custom/admin/legislation/processes_spec.rb
@@ -5,14 +5,14 @@ describe "Admin collaborative legislation", :admin do
     scenario "Shows a download link per process", :no_js do
       process = create(:legislation_process)
 
-      visit admin_legislation_processes_path(filter: "all")
+      visit admin_legislation_processes_path(filter: "all", locale: :es)
       within "#legislation_process_#{process.id}" do
         click_link("Descargar CSV")
       end
 
       header = page.response_headers["Content-Disposition"]
       expect(header).to match(/^attachment;/)
-      expect(header).to match(/filename="process.zip"$/)
+      expect(header).to match(/filename="process_#{process.id}.zip"$/)
     end
   end
 end
