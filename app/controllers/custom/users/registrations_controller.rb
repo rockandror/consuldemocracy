2c88a85670ba4de96db61794c05cfef9c3ebf5da
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-02-06 18:59:56 +0100

    Increase test coverage for custom registrations feature

--- app/controllers/custom/users/registrations_controller.rb
+++ app/controllers/custom/users/registrations_controller.rb
@@ -1,16 +1,15 @@
 require_dependency Rails.root.join("app", "controllers", "users", "registrations_controller").to_s
 
 class Users::RegistrationsController
-  def create
-    build_resource(sign_up_params)
-    if resource.valid?
-      super
-      @activity = ActivityLog.new(activity: "register", result: "ok", payload: resource.email)
-      @activity.save
-    else
-      @activity = ActivityLog.new(activity: "register", result: "error", payload: resource.errors.full_messages)
-      @activity.save
-      render :new
+  after_action :track_activity, only: :create
+
+  private
+
+    def track_activity
+      if resource.persisted?
+        ActivityLog.create!(activity: "register", result: "ok", payload: resource.email)
+      else
+        ActivityLog.create!(activity: "register", result: "error", payload: resource.errors.full_messages)
+      end
     end
-  end
 end
