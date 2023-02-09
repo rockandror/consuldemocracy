cab393cbffabc852060993c48c3957cbcae66c4f
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-02-09 16:33:51 +0100

    Increase test coverage for custom admin tags feature

--- spec/system/custom/admin/stats_spec.rb
+++ spec/system/custom/admin/stats_spec.rb
@@ -76,4 +76,23 @@ describe "Stats", :admin do
       expect(page).to have_selector("ul.pagination")
     end
   end
+
+  context "Tags" do
+    scenario "List all tags, most used first" do
+      sport = create(:tag, name: "sport", taggables: [create(:proposal), create(:budget_investment)])
+      health = create(:tag, name: "health", taggables: [create(:debate)])
+
+      visit tags_admin_stats_path
+
+      within "#tag_#{sport.id}" do
+        expect(page).to have_content "sport"
+        expect(page).to have_content "2"
+      end
+      within "#tag_#{health.id}" do
+        expect(page).to have_content "health"
+        expect(page).to have_content "1"
+      end
+      expect("sport").to appear_before("health")
+    end
+  end
 end
