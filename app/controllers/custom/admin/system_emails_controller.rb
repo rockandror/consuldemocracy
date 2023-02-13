c3d4733ab924d930dc1e53fb0ac9dfce97d2f1fb
taitus <sebastia.roig@gmail.com>
2023-02-13 11:23:00 +0100

    Allow view contact email in system email admin section
    Remove unused param :message for translation key "mailers.contact.intro"

--- app/controllers/custom/admin/system_emails_controller.rb
+++ app/controllers/custom/admin/system_emails_controller.rb
@@ -2,9 +2,26 @@ require_dependency Rails.root.join("app", "controllers", "admin", "system_emails
 
 class Admin::SystemEmailsController
   alias_method :consul_index, :index
+  alias_method :consul_view, :view
 
   def index
     consul_index
     @system_emails[:contact] = %w[view edit_info]
   end
+
+  def view
+    if @system_email == "contact"
+      load_sample_contact
+    else
+      consul_view
+    end
+  end
+
+  private
+
+    def load_sample_contact
+      @subject = "Asunto de ejemplo"
+      @intro = I18n.t("mailers.contact.intro", name: "Juan", email: "juan@mail.com")
+      @message = "Mensaje de ejemplo"
+    end
 end
