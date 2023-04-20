cb95513e45504fe67655789ce8de51e8538e6c9e
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-04-20 13:24:15 +0200

    Log successful and failed login attempts in a separate log file
    We log the login parameter and the request IP address.
    
    Quoting the ENS:
    
    > [op.acc.5.r5.1] Se registrarán los accesos con éxito y los fallidos.

--- spec/controllers/custom/users/sessions_controller_spec.rb
+++ spec/controllers/custom/users/sessions_controller_spec.rb
@@ -3,12 +3,13 @@ require "rails_helper"
 describe Users::SessionsController do
   before do
     request.env["devise.mapping"] = Devise.mappings[:user]
-    allow(User).to receive(:maximum_attempts).and_return(1)
   end
 
   let!(:user) { create(:user, email: "citizen@consul.org", password: "12345678", locale: :es) }
 
   describe "Activity log" do
+    before { allow(Rails.application.config).to receive(:authentication_logs).and_return(true) }
+
     context "when login process is successful" do
       it "tracks the login result" do
         expect do
@@ -32,6 +33,8 @@ describe Users::SessionsController do
       end
 
       it "tracks the user account lock" do
+        allow(User).to receive(:maximum_attempts).and_return(1)
+
         expect do
           post :create, params: { user: { login: "citizen@consul.org", password: "wrong" }}
         end.to change(ActivityLog, :count).by(2)
@@ -46,6 +49,8 @@ describe Users::SessionsController do
   end
 
   describe "Devise lock" do
+    before { allow(User).to receive(:maximum_attempts).and_return(1) }
+
     context "when devise sign in maximum_attempts reached", :with_frozen_time do
       it "locks the user account and sends an email to the account with an unlock link" do
         expect do
@@ -57,6 +62,53 @@ describe Users::SessionsController do
         body = ActionMailer::Base.deliveries.last.body
         expect(body).to have_content "Tu cuenta ha sido bloqueada"
         expect(body).to have_link "Desbloquear mi cuenta"
+      end
+    end
+  end
+
+  describe "access logs" do
+    context "when feature is enabled" do
+      before { allow(Rails.application.config).to receive(:authentication_logs).and_return(true) }
+
+      it "when a sign in process succeeds it calls the authentication logger" do
+        message = "The user citizen@consul.org with IP address: 0.0.0.0 successfully signed in."
+        expect(AuthenticationLogger).to receive(:log).with(message)
+
+        post :create, params: { user: { login: "citizen@consul.org", password: "12345678" }}
+      end
+
+      it "when a sign in process fails it calls the authentication logger" do
+        message = "The user citizen@consul.org with IP address: 0.0.0.0 failed to sign in."
+        expect(AuthenticationLogger).to receive(:log).with(message)
+
+        post :create, params: { user: { login: "citizen@consul.org", password: "wrong" }}
+      end
+
+      it "when maximum attempts is reached it tracks the user account lock" do
+        allow(User).to receive(:maximum_attempts).and_return(1)
+        message_1 = "The user citizen@consul.org with IP address: 0.0.0.0 failed to sign in."
+        message_2 = "The user citizen@consul.org with IP address: 0.0.0.0 reached maximum attempts " \
+                  "and it's temporarily locked."
+        expect(AuthenticationLogger).to receive(:log).once.with(message_1)
+        expect(AuthenticationLogger).to receive(:log).once.with(message_2)
+
+        post :create, params: { user: { login: "citizen@consul.org", password: "wrong" }}
+      end
+    end
+
+    context "when feature is disabled" do
+      before { allow(Rails.application.config).to receive(:authentication_logs).and_return(false) }
+
+      it "when a sign in process succeeds it does not call the authentication logger" do
+        expect(AuthenticationLogger).not_to receive(:log)
+
+        post :create, params: { user: { login: "citizen@consul.org", password: "12345678" }}
+      end
+
+      it "when a sign in process fails it does not call the authentication logger" do
+        expect(AuthenticationLogger).not_to receive(:log)
+
+        post :create, params: { user: { login: "citizen@consul.org", password: "wrong" }}
       end
     end
   end
