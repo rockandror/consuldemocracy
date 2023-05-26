f9a2f08417a2535e38d77e6fcd7965ae448adef2
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-05-26 11:50:28 +0200

    Update the repository for deployments
    The former repository was migrated to the new address
    and it's not available anymore.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :application, "consul"
 set :deploy_to, deploysecret(:deploy_to)
 set :ssh_options, port: deploysecret(:ssh_port)
 
-set :repo_url, "https://git.nubarron.teide.int/ENREDA_SCA/cabildoabierto-consul.git"
+set :repo_url, "https://git.nubarron.teide.int/shs-consul/cabildoabierto-consul.git"
 
 set :revision, `git rev-parse --short #{fetch(:branch)}`.strip
 
