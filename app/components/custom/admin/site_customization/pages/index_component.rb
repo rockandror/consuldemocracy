d400cc123bac83eacc6c31be547920c5f8c2adf7
Javi Martín <javim@elretirao.net>
2023-07-21 16:42:36 +0200

    Apply 2.0.0 changes to custom admin pages index
    We're moving it to a component like it was done for the original code in
    commit f9d334fb3, and we're applying the other changes in the code.

---
+++ app/components/custom/admin/site_customization/pages/index_component.rb
@@ -0,0 +1,13 @@
+class Admin::SiteCustomization::Pages::IndexComponent < ApplicationComponent; end
+
+require_dependency Rails.root.join("app", "components", "admin", "site_customization", "pages", "index_component").to_s
+
+class Admin::SiteCustomization::Pages::IndexComponent
+  attr_reader :search
+  alias_method :consul_initialize, :initialize
+
+  def initialize(pages, search: {})
+    consul_initialize(pages)
+    @search = search
+  end
+end
