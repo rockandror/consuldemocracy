db7873b9886335c84e260dd747a10b5b14ab7f24
taitus <sebastia.roig@gmail.com>
2023-09-01 10:28:33 +0200

    Add optional fields to registration form

---
+++ app/controllers/custom/account_controller.rb
@@ -0,0 +1,16 @@
+require_dependency Rails.root.join("app", "controllers", "account_controller").to_s
+
+class AccountController
+
+  private
+
+    alias_method :consul_allowed_params, :allowed_params
+
+    def allowed_params
+      if @account.organization?
+        consul_allowed_params
+      else
+        consul_allowed_params + [:date_of_birth, :geozone_id, :gender]
+      end
+    end
+end
