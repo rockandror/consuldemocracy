d9e161d89d26d120353fc83448b92b4cd82debfb
Senén Rodero Rodríguez <15726+Senen@users.noreply.github.com>
2023-09-27 15:43:02 +0200

    Drop ActivityLog model and related code
    As now we are going to use a log file to store successful and failed sign
    in attempts.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_23_090028) do
+ActiveRecord::Schema.define(version: 2023_08_23_094027) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -62,16 +62,6 @@ ActiveRecord::Schema.define(version: 2023_05_23_090028) do
     t.datetime "updated_at"
     t.index ["actionable_id", "actionable_type"], name: "index_activities_on_actionable_id_and_actionable_type"
     t.index ["user_id"], name: "index_activities_on_user_id"
-  end
-
-  create_table "activity_logs", force: :cascade do |t|
-    t.string "activity"
-    t.string "result"
-    t.string "model"
-    t.integer "model_id"
-    t.text "payload"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "admin_notification_translations", id: :serial, force: :cascade do |t|
