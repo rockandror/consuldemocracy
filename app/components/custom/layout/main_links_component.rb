e3422653fab781f4dad66c9e22d6998a11c7e74a
Diego Calvo Castillo <diego.calvo@enreda.coop>
2023-12-26 13:11:08 +0100

    change link

--- app/components/custom/layout/main_links_component.rb
+++ app/components/custom/layout/main_links_component.rb
@@ -12,7 +12,7 @@ class Layout::MainLinksComponent < ApplicationComponent
           collaboration:
             { path: "/_participacion_que-hacemos", img: "participacion-colaboracion.png" },
           open_data:
-            { path: "https://www.tenerifedata.com/", img: "datos-abiertos.png", target: "_blank" },
+            { path: "https://datos.tenerife.es/es/", img: "datos-abiertos.png", target: "_blank" },
           ethics:
             { path: "/_etica_codigo-de-buen-gobierno-y-seguimiento", icon: "balance-scale" }
         }
