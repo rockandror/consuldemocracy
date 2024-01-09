0e4fd866e782cb8ce61340f1a5c3550269bcfee3
Senén Rodero Rodríguez <senenrodero@gmail.com>
2024-01-09 13:01:15 +0100

    Allow administrators to define the cookies vendors the application uses

--- config/routes/custom.rb
+++ config/routes/custom.rb
@@ -1,3 +1,9 @@
 # Add here routes for custom features
 get "contact", to: "contact#new", as: "new_contact"
 post "contact", to: "contact#create", as: "contact"
+
+namespace :admin do
+  namespace :cookies do
+    resources :vendors, except: [:index]
+  end
+end
