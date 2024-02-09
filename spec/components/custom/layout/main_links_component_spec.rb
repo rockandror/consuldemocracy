c33fffe549c57d2188f3ad2f966460a1df946f73
Senén Rodero Rodríguez <senenrodero@gmail.com>
2024-02-09 12:55:19 +0100

    Fix spec broken at e3422653fa

--- spec/components/custom/layout/main_links_component_spec.rb
+++ spec/components/custom/layout/main_links_component_spec.rb
@@ -7,7 +7,7 @@ describe Layout::MainLinksComponent, type: :component do
     expect(page).to have_link "Cabildo Abierto", href: "/_cabildo_que-es-el-cabildo-abierto"
     expect(page).to have_link "Transparencia", href: "https://transparencia.tenerife.es/"
     expect(page).to have_link "Participación y colaboración", href: "/_participacion_que-hacemos"
-    expect(page).to have_link "Datos Abiertos", href: "https://www.tenerifedata.com/"
+    expect(page).to have_link "Datos Abiertos", href: "https://datos.tenerife.es/es/"
     expect(page).to have_link "Ética Pública", href: "/_etica_codigo-de-buen-gobierno-y-seguimiento"
   end
 end
